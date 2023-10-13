0fa4d7f337d4cb477b974a3cb58ced44559d044e
v012345 <v138468@gmail.com>
2023-10-13 10:22:17 +0800

    fixed 去除 bom 功能 bug

--- scripts/RemoveBOM.lua
+++ scripts/RemoveBOM.lua
@@ -23,10 +23,10 @@ for _, file in ipairs(allFiles) do
         local bom = string.format("%x%x%x", string.byte(c, 1, 3))
         if string.lower(bom) == "efbbbf" then
             c = string.sub(c, 4, #c)
+            f = io.open(file, "w") or error()
+            f:write(c)
+            f:close()
+            print(file)
         end
-        f = io.open(file, "w") or error()
-        f:write(c)
-        f:close()
-        print(file)
     end
 end
