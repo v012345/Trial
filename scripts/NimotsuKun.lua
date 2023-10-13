898a343e9ef32a23b9ea82ba0a913fdea40c506c
v012345 <v138468@gmail.com>
2023-10-13 11:51:36 +0800

    优化推箱子的脚本

--- scripts/NimotsuKun.lua
+++ scripts/NimotsuKun.lua
@@ -8,23 +8,27 @@ local OBJ = {
     MAN, MAN_ON_GRASS, HORSE, UNICORN, GRASS, LOG, [0] = "  "
 }
 print("a,s,d,w to move")
-local Ground = {
-    { 6, 6, 6, 6, 6, 6, 6, 6 },
-    { 6, 0, 5, 5, 0, 0, 0, 6 },
-    { 6, 0, 0, 0, 0, 0, 0, 6 },
-    { 6, 0, 0, 0, 0, 0, 0, 6 },
-    { 6, 6, 6, 6, 6, 6, 6, 6 },
+local factory = {
+    { 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 },
+    { 6, 0, 5, 0, 0, 0, 0, 0, 0, 6 },
+    { 6, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+    { 6, 0, 0, 0, 0, 0, 0, 0, 0, 6 },
+    { 6, 0, 0, 0, 6, 6, 6, 0, 0, 6 },
+    { 6, 0, 0, 0, 6, 0, 6, 5, 0, 6 },
+    { 6, 0, 0, 0, 6, 0, 6, 0, 0, 6 },
+    { 6, 6, 6, 6, 6, 0, 6, 6, 6, 6 },
 }
-local position = { x = 4, y = 2 }
+local worker_pos = { x = 4, y = 2 }
 local HORSES = {
     { x = 3, y = 3 },
     { x = 3, y = 4 }
 }
 
+-- 渲染地图
 local function draw()
-    for x, row in ipairs(Ground) do
+    for x, row in ipairs(factory) do
         for y, cell in ipairs(row) do
-            if position.x == x and position.y == y then
+            if worker_pos.x == x and worker_pos.y == y then
                 if cell == 5 then
                     io.write(OBJ[2])
                 else
@@ -51,81 +55,71 @@ local function draw()
         io.write("\n")
     end
 end
-local function is_clear()
+local function is_horse(x, y)
     for _, horse in ipairs(HORSES) do
-        if Ground[horse.x][horse.y] ~= 5 then
+        if horse.x == x and horse.y == y then
+            return true, horse
+        end
+    end
+    return false, {}
+end
+local function can_stand(x, y)
+    if factory[x][y] == 6 then
+        return false
+    end
+    return true
+end
+-- 通关
+local function check_clear()
+    for _, horse in ipairs(HORSES) do
+        if factory[horse.x][horse.y] ~= 5 then
             return false
         end
     end
     return true
 end
 local function deal_input(input)
-    local old_x = position.x
-    local old_y = position.y
+    local old_x = worker_pos.x
+    local old_y = worker_pos.y
+    local dx = 0
+    local dy = 0
     if input == "a" then
-        position.y = position.y - 1
+        dy = -1
     elseif input == "s" then
-        position.x = position.x + 1
+        dx = 1
     elseif input == "d" then
-        position.y = position.y + 1
+        dy = 1
     elseif input == "w" then
-        position.x = position.x - 1
+        dx = -1
     end
-    for _, horse in ipairs(HORSES) do
-        if horse.x == position.x and horse.y == position.y then
-            local h_old_x = horse.x
-            local h_old_y = horse.y
-            if input == "a" then
-                horse.y = horse.y - 1
-            elseif input == "s" then
-                horse.x = horse.x + 1
-            elseif input == "d" then
-                horse.y = horse.y + 1
-            elseif input == "w" then
-                horse.x = horse.x - 1
+    local isHorse, horse = is_horse(old_x + dx, old_y + dy)
+    if isHorse then
+        if can_stand(horse.x + dx, horse.y + dy) then
+            if not is_horse(horse.x + dx, horse.y + dy) then
+                worker_pos.x = old_x + dx
+                worker_pos.y = old_y + dy
+                horse.x = horse.x + dx
+                horse.y = horse.y + dy
             end
-            if horse.x < 2 or horse.x > #Ground - 1 then
-                horse.x = h_old_x
-                position.x = old_x
-                return
-            end
-            if horse.y < 2 or horse.y > #Ground[1] - 1 then
-                horse.y = h_old_y
-                position.y = old_y
-                return
-            end
-            for _, other_horse in ipairs(HORSES) do
-                if other_horse ~= horse then
-                    if horse.x == other_horse.x and horse.y == other_horse.y then
-                        horse.y = h_old_y
-                        position.y = old_y
-                        return
-                    end
-                end
-            end
-            break
         end
-    end
-    if position.x < 2 or position.x > #Ground - 1 then
-        position.x = old_x
-    end
-    if position.y < 2 or position.y > #Ground[1] - 1 then
-        position.y = old_y
+    elseif can_stand(old_x + dx, old_y + dy) then
+        worker_pos.x = old_x + dx
+        worker_pos.y = old_y + dy
     end
 end
 draw()
 while true do
+    if check_clear() then
+        print("Congratulation!")
+        break
+    end
     local input = string.char(_getch())
-    local old_x = position.x
-    local old_y = position.y
+    local old_x = worker_pos.x
+    local old_y = worker_pos.y
     deal_input(input)
-    if not (old_x == position.x and old_y == position.y) then
+    if not (old_x == worker_pos.x and old_y == worker_pos.y) then
         SetConsoleCursorPosition(0, 2)
         draw()
-        if is_clear() then
-            print("Congratulation!")
-            break
-        end
     end
 end
-os.execute("pause")
+-- os.execute("pause")
