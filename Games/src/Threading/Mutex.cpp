1c427a6fd4037b6c8ea702b73a59a53d58bcb546
v138468 <v138468@gmail.com>
2023-10-15 15:19:09 +0800

    改成 vscode 友好的写法

--- Games/src/Threading/Mutex.cpp
+++ Games/src/Threading/Mutex.cpp
@@ -1,51 +1,66 @@
 ﻿#include <windows.h>
 #undef min
 #undef max
+#include "GameLib/Base/Impl/ReferenceType.h"
 #include "GameLib/GameLib.h"
 #include "GameLib/Threading/Mutex.h"
-#include "GameLib/Base/Impl/ReferenceType.h"
 
-namespace GameLib{
-namespace Threading{
+namespace GameLib {
+namespace Threading {
 
-//要检测多个解锁，多个锁等，内容为信号量。
-//在发行版中切换到CriticalSection可能很好。
-class Mutex::Impl : public ReferenceType{
-public:
-	Impl() : mHandle( 0 ){
-		mHandle = CreateSemaphore( NULL, 1, 1, NULL );
-	}
-	~Impl(){
-		CloseHandle( mHandle );
-	}
-	void lock(){
-		WaitForSingleObject( mHandle, INFINITE );
-	}
-	void unlock(){
-		LONG prev;
-		ReleaseSemaphore( mHandle, 1, &prev );
-		STRONG_ASSERT( prev == 0 && "unlock() is called twice! Check lock-unlock pair." );
-	}
-	HANDLE mHandle;
+// 要检测多个解锁，多个锁等，内容为信号量。
+// 在发行版中切换到CriticalSection可能很好。
+class Mutex::Impl : public ReferenceType {
+  public:
+    Impl() : mHandle(0) { mHandle = CreateSemaphore(NULL, 1, 1, NULL); }
+    ~Impl() { CloseHandle(mHandle); }
+    void lock() { WaitForSingleObject(mHandle, INFINITE); }
+    void unlock() {
+        LONG prev;
+        ReleaseSemaphore(mHandle, 1, &prev);
+        STRONG_ASSERT(prev == 0 && "unlock() is called twice! Check lock-unlock pair.");
+    }
+    HANDLE mHandle;
 };
 
-Mutex Mutex::create(){
-	Mutex r;
-	r.mImpl = NEW Impl();
-	return r;
+Mutex Mutex::create() {
+    Mutex r;
+    r.mImpl = NEW Impl();
+    return r;
 }
 
-void Mutex::lock(){
-	mImpl->lock();
+void Mutex::lock() { mImpl->lock(); }
+
+void Mutex::unlock() { mImpl->unlock(); }
+
+Mutex::Mutex() : mImpl(0) {}
+
+Mutex::Mutex(const Mutex& o) : mImpl(o.mImpl) {
+    if (mImpl) { mImpl->refer(); }
 }
 
-void Mutex::unlock(){
-	mImpl->unlock();
+Mutex::~Mutex() { release(); }
+
+void Mutex::release() {
+    if (mImpl) {
+        mImpl->release();
+        if (mImpl->referenceCount() == 0) { SAFE_DELETE(mImpl); }
+    }
+    mImpl = 0;
 }
 
-#define TYPE Mutex
-#include "GameLib/Base/Impl/ReferenceTypeTemplate.h"
+Mutex& Mutex::operator=(const Mutex& o) {
+    release();
+    mImpl = o.mImpl;
+    if (mImpl) { mImpl->refer(); }
+    return *this;
+}
 
+bool Mutex::operator==(const Mutex& o) const { return (mImpl == o.mImpl); }
 
-} //namespace Threading
-} //namespace GameLib
+bool Mutex::operator!=(const Mutex& o) const { return (mImpl != o.mImpl); }
+
+Mutex::operator void*() const { return (mImpl == 0) ? 0 : reinterpret_cast<void*>(~0); }
+
+} // namespace Threading
+} // namespace GameLib
