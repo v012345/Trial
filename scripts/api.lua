9a05e7e879a68ba47d287679e8745bd8fbdd0354
v012345 <v138468@gmail.com>
2023-10-16 18:28:16 +0800

    给新方法加入 api 提示

--- scripts/api.lua
+++ scripts/api.lua
@@ -9,3 +9,18 @@ function SetConsoleCursorPosition(column, line) end
 function _getch() return 0 end
 
 CMAKE_SOURCE_DIR = "源码目录"
+
+Impl = {}
+---在窗口的指定位置画一个点
+---@param w integer 第几行
+---@param h integer 第几列
+---@param c integer 什么颜色
+function Impl:vram(w, h, c) end
+
+---窗口的高
+---@return integer
+function Impl:height() return 0 end
+
+---窗口的宽
+---@return integer
+function Impl:width() return 0 end
--- scripts/main.lua
+++ scripts/main.lua
@@ -5,14 +5,6 @@
 --     end, function(msg)
 --     print(msg)
 -- end)
-
-local file = io.open(CMAKE_SOURCE_DIR .. "/log/log.txt", "w") or error()
-file:write("ll22222222222llll");
-file:close()
-
-print("lua:jifjisojfseifjsijfii")
-print("lua:", Impl, type(getmetatable(Impl)), Impl.height, Impl:height())
-
 for i = 1, 10, 1 do
     for j = 1, 10, 1 do
         Impl:vram(i, j, 0xFF0000)
