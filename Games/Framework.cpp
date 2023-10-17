511aeea18f3790f8070290d9dca37c45a4d72044
v012345 <v138468@gmail.com>
2023-10-17 08:55:03 +0800

    给 package 加入新的搜索路径

--- Games/Framework.cpp
+++ Games/Framework.cpp
@@ -237,10 +237,28 @@ namespace GameLib {
         L = luaL_newstate();
         luaL_openlibs(L);
         luaopen_lfs(L);
+#ifdef CMAKE_SOURCE_DIR
         lua_pushstring(L, CMAKE_SOURCE_DIR);
         lua_setglobal(L, "CMAKE_SOURCE_DIR");
+#endif
+
+#ifdef LUA_PACKAGE_PATH
+        lua_getglobal(L, "package");
+        lua_getfield(L, -1, "path");
+        const char* path = lua_tostring(L, -1);
+        char* new_path = (char*)malloc(sizeof(char) * (strlen(path) + strlen(";" LUA_PACKAGE_PATH) + 1));
+        memcpy(new_path, path, strlen(path));
+        memcpy(new_path + strlen(path), ";" LUA_PACKAGE_PATH, strlen(";" LUA_PACKAGE_PATH) + 1);
+        lua_pop(L, 1);
+        lua_pushstring(L, "path");
+        lua_pushstring(L, new_path);
+        lua_settable(L, -3);
+        free(new_path);
+#endif
         luaopen_Impl(L, gImpl);
+#ifdef LUA_MAIN_SCRIPT
         luaL_dofile(L, LUA_MAIN_SCRIPT);
+#endif
     }
 
     void Framework::destroy() { SAFE_DELETE(gImpl); }
