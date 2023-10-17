1d6c7acd6f52e1831d652b8338266e15fd486a05
v138468 <v138468@gmail.com>
2023-10-17 21:13:34 +0800

    测试 NimotsuKunAlphaTest 项目 可行

--- example/Image.h
+++ example/Image.h
@@ -1,18 +1,23 @@
 ﻿#ifndef INCLUDED_IMAGE_H
 #define INCLUDED_IMAGE_H
 
-class Image {
-  public:
-    Image(const char* filename);
-    ~Image();
-    int width() const;
-    int height() const;
-    void draw(int dstX, int dstY, int srcX, int srcY, int width, int height) const;
-
-  private:
-    int mWidth;
-    int mHeight;
-    unsigned* mData;
+class Image{
+public:
+	Image( const char* filename );
+	~Image();
+	int width() const;
+	int height() const;
+	void draw( 
+		int dstX, 
+		int dstY, 
+		int srcX, 
+		int srcY, 
+		int width, 
+		int height ) const;
+private:
+	int mWidth;
+	int mHeight;
+	unsigned* mData;
 };
 
 #endif
--- example/main.cpp
+++ example/main.cpp
@@ -1,18 +1,65 @@
 ﻿#include "../Games/Framework.h"
-#include "Image.h"
+using namespace GameLib;
 
-bool gFirst = true;
-Image* gForeground;
-Image* gBackground;
+#include "File.h"
+#include "State.h"
 
+// 函数原型
+void mainLoop();
+
+// 全局变量
+State* gState = 0;
+
+// 用户封装函数。内容被抛出给mainLoop（）
 namespace GameLib {
-    void Framework::update() {
-        if (gFirst) {
-            gFirst = false;
-            gForeground = new Image(CMAKE_CURRENT_SOURCE_DIR "forground.dds");
-            gBackground = new Image(CMAKE_CURRENT_SOURCE_DIR "background.dds");
-        }
-        gBackground->draw(0, 0, 0, 0, gBackground->width(), gBackground->height());
-        gForeground->draw(0, 0, 0, 0, gForeground->width(), gForeground->height());
-    }
+    void Framework::update() { mainLoop(); }
 } // namespace GameLib
+
+void mainLoop() {
+    // ×按钮被按下了吗？
+    if (Framework::instance().isEndRequested()) {
+        if (gState) {
+            delete gState;
+            gState = 0;
+        }
+        return;
+    }
+    // 初始化第一帧。绘制第一个状态并完成。
+    if (!gState) {
+        File file(CMAKE_CURRENT_SOURCE_DIR "stageData.txt");
+        if (!(file.data())) { // 没有数据！
+            cout << "stage file could not be read." << endl;
+            return;
+        }
+        gState = new State(file.data(), file.size());
+        // 第一绘制
+        gState->draw();
+        return; // 结束
+    }
+    bool cleared = false;
+    // 主循环
+    // 清除检测
+    if (gState->hasCleared()) { cleared = true; }
+    // 获取输入
+    cout << "a:left s:right w:up z:down. command?" << endl; // 操作说明
+    char input;
+    cin >> input;
+    // 结束判断
+    if (input == 'q') {
+        delete gState;
+        gState = 0;
+        Framework::instance().requestEnd();
+        return;
+    }
+    // 更新
+    gState->update(input);
+    // 绘制
+    gState->draw();
+
+    if (cleared) {
+        // 庆祝消息
+        cout << "Congratulation! you win." << endl;
+        delete gState;
+        gState = 0;
+    }
+}
