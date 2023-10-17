ad686558b7adb0c888bfa564513ecfbc33335d2d
v012345 <v138468@gmail.com>
2023-10-17 17:20:54 +0800

    正确切图

--- scripts/update.lua
+++ scripts/update.lua
@@ -1,25 +1,21 @@
 xpcall(function()
     os.execute("chcp 65001 > NUL")
     print("Hello 👋")
-    -- print(.height)
-    local p = ReadPngFile(CMAKE_SOURCE_DIR .. "/res/shengyue.png")
-    -- for index, value in ipairs(p.RGB) do
-    --     print(index, string.format("%x", value))
-    -- end
-    -- print(p.height, p.width, #p.RGB)
-    -- for i = 1, p.height, 1 do
-    --     for j = 1, p.width, 1 do
-    --         io.write(string.format("%x", p.RGB[(i - 1) * p.width + j]), "\t")
-    --     end
-    --     io.write("\n")
-    -- end
-
-    print("Hello1 👋")
-    function MainLoop()
-        for i = 1, p.height, 1 do
-            for j = 1, p.width, 1 do
-                Impl:vram(i - 1, j - 1, p.RGB[(i - 1) * p.width + j])
+    local nimotsuKunImage = ReadPngFile(CMAKE_SOURCE_DIR .. "/res/nimotsuKunImage2.png")
+    ---@param IMAGE image
+    ---@param TYPE integer
+    local function drawCell(IMAGE, TYPE, atX, atY)
+        for i = 1, IMAGE.height, 1 do
+            local y = 0
+            for j = (TYPE - 1) * 32 + 1, TYPE * 32, 1 do
+                Impl:vram(atX + i - 1, atY + y, IMAGE.RGB[i * IMAGE.width + j])
+                y = y + 1
             end
+        end
+    end
+    function MainLoop()
+        for i = 1, 5, 1 do
+            drawCell(nimotsuKunImage, i, i * 32, i * 32)
         end
     end
 end, function(msg)
