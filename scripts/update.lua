605c6fe8c2ae8bc5f9b0f1da0fe615ee3d4ceeec
v012345 <v138468@gmail.com>
2023-10-18 13:53:53 +0800

    尝试去画透明图片

--- scripts/update.lua
+++ scripts/update.lua
@@ -1,7 +1,7 @@
 xpcall(function()
     os.execute("chcp 65001 > NUL")
     print("Hello 👋")
-    local nimotsuKunImage = ReadPngFile(CMAKE_SOURCE_DIR .. "/res/nimotsuKunImage2.png")
+    local nimotsuKunImage = ReadPngFile(CMAKE_SOURCE_DIR .. "/res/transparent.png")
     ---@param IMAGE image
     ---@param TYPE integer
     local function drawCell(IMAGE, TYPE, atX, atY)
@@ -13,10 +13,20 @@ xpcall(function()
             end
         end
     end
-    function MainLoop()
-        for i = 1, 5, 1 do
-            drawCell(nimotsuKunImage, i, i * 32, i * 32)
+
+    ---@param IMAGE image
+    local function drawImage(IMAGE, atX, atY)
+        for i = 1, IMAGE.height, 1 do
+            for j = 1, IMAGE.width, 1 do
+                Impl:vram(atX + i - 1, atY + j - 1, IMAGE.RGB[i * IMAGE.width + j])
+            end
         end
+    end
+    function MainLoop()
+        -- for i = 1, 5, 1 do
+        -- drawCell(nimotsuKunImage, i, i * 32, i * 32)
+        -- end
+        drawImage(nimotsuKunImage, 0, 0)
     end
 end, function(msg)
     print(msg)
