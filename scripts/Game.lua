b3a9d4f46c2eff32a5ecb92ece5c66079f018dd1
v012345 <v138468@gmail.com>
2023-10-20 12:08:46 +0800

    渲染数据加载

--- scripts/Game.lua
+++ scripts/Game.lua
@@ -1,3 +1,31 @@
+function Game:drawImage(IMAGE, atX, atY, isBG)
+    for y, row in ipairs(IMAGE) do
+        for x, argb in ipairs(row) do
+            if isBG then
+                Impl:vram(atX + x - 1, atY + y - 1, argb)
+            else
+                local srcA = (argb & 0xff000000) >> 24
+                local srcR = argb & 0xff0000;
+                local srcG = argb & 0x00ff00;
+                local srcB = argb & 0x0000ff;
+                local dst = Impl:colorAt(atX + x - 1, atY + y - 1)
+                local dstR = dst & 0xff0000;
+                local dstG = dst & 0x00ff00;
+                local dstB = dst & 0x0000ff;
+                local r = (srcR - dstR) * srcA // 255 + dstR;
+                local g = (srcG - dstG) * srcA // 255 + dstG;
+                local b = (srcB - dstB) * srcA // 255 + dstB;
+                -- 如果大于255则设置为255
+                r = (r > 0xff0000) and 0xff0000 or r;
+                g = (g > 0x00ff00) and 0x00ff00 or g;
+                b = (b > 0x0000ff) and 0x0000ff or b;
+                dst = (r & 0xff0000) | (g & 0x00ff00) | b;
+                Impl:vram(atX + x - 1, atY + y - 1, dst)
+            end
+        end
+    end
+end
+
 function Game:loadBackground(map)
     self.background = {}
     for _, v in ipairs(map) do
@@ -91,5 +119,34 @@ function Game:drawBackground()
 end
 
 function Game:loadRenderImage(path)
-
+    local png = ReadPngFile(path)
+    local function slice(IMAGE, fromX, toX)
+        local res = {}
+        for y, xRow in ipairs(IMAGE) do
+            local row = {}
+            for x, argb in ipairs(xRow) do
+                if fromX <= x and x <= toX then
+                    row[#row + 1] = argb
+                end
+            end
+            res[#res + 1] = row
+        end
+        return res
+    end
+    local empty = {}
+    for i = 1, 32 do
+        local row = {}
+        for j = 1, 32 do
+            row[#row + 1] = 0
+        end
+        empty[#empty + 1] = row
+    end
+    self.renderEntity = {}
+    self.renderEntity[ENUM.player] = slice(png, 1 + 32 * 0, 32 + 32 * 0)
+    self.renderEntity[ENUM.wall] = slice(png, 1 + 32 * 1, 32 + 32 * 1)
+    self.renderEntity[ENUM.box] = slice(png, 1 + 32 * 2, 32 + 32 * 2)
+    self.renderEntity[ENUM.goal] = slice(png, 1 + 32 * 3, 32 + 32 * 3)
+    self.renderEntity[ENUM.ground] = slice(png, 1 + 32 * 4, 32 + 32 * 4)
+    self.renderEntity[ENUM.empty] = empty
+    -- self:drawImage(self.renderEntity[ENUM.box], 0, 0, true)
 end
--- scripts/StartGame.lua
+++ scripts/StartGame.lua
@@ -22,33 +22,7 @@ xpcall(function()
         Game.PreviousTime[i] = Framework.time();
     end
     Game.Counter = 0;
-    function Game.drawImage(IMAGE, atX, atY, isBG)
-        for y, row in ipairs(IMAGE) do
-            for x, argb in ipairs(row) do
-                if isBG then
-                    Impl:vram(atX + x - 1, atY + y - 1, argb)
-                else
-                    local srcA = (argb & 0xff000000) >> 24
-                    local srcR = argb & 0xff0000;
-                    local srcG = argb & 0x00ff00;
-                    local srcB = argb & 0x0000ff;
-                    local dst = Impl:colorAt(atX + x - 1, atY + y - 1)
-                    local dstR = dst & 0xff0000;
-                    local dstG = dst & 0x00ff00;
-                    local dstB = dst & 0x0000ff;
-                    local r = (srcR - dstR) * srcA // 255 + dstR;
-                    local g = (srcG - dstG) * srcA // 255 + dstG;
-                    local b = (srcB - dstB) * srcA // 255 + dstB;
-                    -- 如果大于255则设置为255
-                    r = (r > 0xff0000) and 0xff0000 or r;
-                    g = (g > 0x00ff00) and 0x00ff00 or g;
-                    b = (b > 0x0000ff) and 0x0000ff or b;
-                    dst = (r & 0xff0000) | (g & 0x00ff00) | b;
-                    Impl:vram(atX + x - 1, atY + y - 1, dst)
-                end
-            end
-        end
-    end
+    
 
     Game.entity = require "ConertStagesToImage"
 
