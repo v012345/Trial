8033f79e21ce182c513040413472c5228ab23e05
v138468 <v138468@gmail.com>
2023-10-22 12:00:16 +0800

    调整结构

--- scripts/Main.lua
+++ scripts/Main.lua
@@ -2,11 +2,29 @@ xpcall(function()
     os.execute("chcp 65001 > NUL")
     print("Hello 👋")
     require "init"
-    SceneManager:init()
     ViewManager:init()
     SceneManager:load("Title")
     -- Game:dumpBackground()
     -- Game:dumpMapWithEmoji()
+    function MainLoop()
+        xpcall(function()
+            local currentTime = Framework.time()
+            -- 这里有溢出问题
+            while currentTime - Framework.PreviousTime[10] < 16 do
+                currentTime = Framework.time()
+                Framework.sleep(1);
+            end
+            for i = 1, 9, 1 do
+                Framework.PreviousTime[i] = Framework.PreviousTime[i + 1];
+            end
+            Framework.PreviousTime[10] = currentTime
+            SceneManager:update()
+            ViewManager:update()
+            Framework:showFPS()
+        end, function(msg)
+            print(msg)
+        end)
+    end
 end, function(msg)
     print(msg)
 end)
--- scripts/init.lua
+++ scripts/init.lua
@@ -1,8 +1,6 @@
 require "Entity"
--- require "RenderEntity"
 require "Engine"
 require "Enum"
 require "Keyboard"
-require "MainLoop"
 require "SceneManager"
 require "ViewManager"
