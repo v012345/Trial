42454240c2a76f5310c75f54f28ace0e022bd079
v012345 <v138468@gmail.com>
2023-10-25 17:37:03 +0800

    格式化

--- example/Sequence/Game/Parent.cpp
+++ example/Sequence/Game/Parent.cpp
@@ -1,102 +1,82 @@
-﻿#include "GameLib/GameLib.h"
-#include "Sequence/Parent.h"
+﻿#include "Sequence/Parent.h"
+#include "File.h"
+#include "Game/State.h"
+#include "GameLib/GameLib.h"
+#include "Sequence/Ending.h"
 #include "Sequence/Game/Parent.h"
 #include "Sequence/Game/Ready.h"
-#include "Sequence/Title.h"
 #include "Sequence/GameOver.h"
-#include "Sequence/Ending.h"
-#include "Game/State.h"
-#include "File.h"
+#include "Sequence/Title.h"
 #include <sstream>
 
-namespace Sequence{
-namespace Game{
+namespace Sequence {
+    namespace Game {
 
-Parent::Parent( GrandParent::Mode mode ) : 
-mState( 0 ),
-mStageID( 0 ),
-mLife( INITIALI_LIFE_NUMBER ),
-mChild( 0 ){
-	if ( mode == GrandParent::MODE_1P ){
-		mStageID = 1;
-	}else{
-		mStageID = 0;
-	}
-	//一开始Ready
-	mChild = new Ready();
-}
+        Parent::Parent(GrandParent::Mode mode) : mState(0), mStageID(0), mLife(INITIALI_LIFE_NUMBER), mChild(0) {
+            if (mode == GrandParent::MODE_1P) {
+                mStageID = 1;
+            } else {
+                mStageID = 0;
+            }
+            // 一开始Ready
+            mChild = new Ready();
+        }
 
-Parent::~Parent(){
-	SAFE_DELETE( mState );
-	SAFE_DELETE( mChild );
-}
+        Parent::~Parent() {
+            SAFE_DELETE(mState);
+            SAFE_DELETE(mChild);
+        }
 
-Base* Parent::update( GrandParent* ){
-	Base* next = this;
-	Base* nextChild = mChild->update( this );
-	//迁移判断
-	if ( nextChild != mChild ){
-		Game::Child* casted = dynamic_cast< Game::Child* >( nextChild );
-		if ( casted ){
-			SAFE_DELETE( mChild );
-			mChild = casted;
-		}else{
-			next = nextChild;
-		}
-		casted = 0;
-	}
-	nextChild = 0; //以防万一
+        Base* Parent::update(GrandParent*) {
+            Base* next = this;
+            Base* nextChild = mChild->update(this);
+            // 迁移判断
+            if (nextChild != mChild) {
+                Game::Child* casted = dynamic_cast<Game::Child*>(nextChild);
+                if (casted) {
+                    SAFE_DELETE(mChild);
+                    mChild = casted;
+                } else {
+                    next = nextChild;
+                }
+                casted = 0;
+            }
+            nextChild = 0; // 以防万一
 
-	return next;
-}
+            return next;
+        }
 
-State* Parent::state(){
-	return mState;
-}
+        State* Parent::state() { return mState; }
 
-bool Parent::hasFinalStageCleared() const {
-	return ( mStageID > FINAL_STAGE );
-}
+        bool Parent::hasFinalStageCleared() const { return (mStageID > FINAL_STAGE); }
 
-int Parent::lifeNumber() const {
-	return mLife;
-}
+        int Parent::lifeNumber() const { return mLife; }
 
-//将Parent::Mode转换为Parent::Mode。不要向下游序列显示父级。
-Parent::Mode Parent::mode() const {
-	Mode r = MODE_NONE;
-	switch ( GrandParent::instance()->mode() ){
-		case GrandParent::MODE_1P: r = MODE_1P; break;
-		case GrandParent::MODE_2P: r = MODE_2P; break;
-		default: ASSERT( false ); break;
-	}
-	return r;
-}
+        // 将Parent::Mode转换为Parent::Mode。不要向下游序列显示父级。
+        Parent::Mode Parent::mode() const {
+            Mode r = MODE_NONE;
+            switch (GrandParent::instance()->mode()) {
+                case GrandParent::MODE_1P: r = MODE_1P; break;
+                case GrandParent::MODE_2P: r = MODE_2P; break;
+                default: ASSERT(false); break;
+            }
+            return r;
+        }
 
-void Parent::startLoading(){
-	SAFE_DELETE( mState );
-	mState = new State( mStageID );
-}
+        void Parent::startLoading() {
+            SAFE_DELETE(mState);
+            mState = new State(mStageID);
+        }
 
-void Parent::drawState() const {
-	mState->draw();
-}
+        void Parent::drawState() const { mState->draw(); }
 
-Parent::PlayerID Parent::winner() const {
-	return mWinner;
-}
+        Parent::PlayerID Parent::winner() const { return mWinner; }
 
-void Parent::setWinner( PlayerID id ){
-	mWinner = id;
-}
+        void Parent::setWinner(PlayerID id) { mWinner = id; }
 
-void Parent::goToNextStage(){
-	++mStageID;
-}
+        void Parent::goToNextStage() { ++mStageID; }
 
-void Parent::reduceLife(){
-	--mLife;
-}
+        void Parent::reduceLife() { --mLife; }
 
-} //namespace Game
-} //namespace Sequence
+    } // namespace Game
+} // namespace Sequence
