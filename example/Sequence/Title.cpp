fcb03b8e142f71f67ffc95d861f6fcd114f4419a
v012345 <v138468@gmail.com>
2023-10-25 16:52:06 +0800

    格式化

--- example/Sequence/Title.cpp
+++ example/Sequence/Title.cpp
@@ -2,56 +2,53 @@
 #include "Games/Framework.h"
 using namespace GameLib;
 
-#include "Sequence/Title.h"
 #include "Sequence/Parent.h"
-#include "Sequence/Game/Parent.h"
+#include "Sequence/Title.h"
+//
 #include "Image.h"
 #include "Pad.h"
+#include "Sequence/Game/Parent.h"
 
-namespace Sequence{
+namespace Sequence {
 
-Title::Title() : mImage( 0 ), mCursorPosistion( 0 ){
-	mImage = new Image( CMAKE_CURRENT_SOURCE_DIR"data/image/dummy.dds" );
-}
+    Title::Title() : mImage(0), mCursorPosistion(0) { mImage = new Image(CMAKE_CURRENT_SOURCE_DIR "data/image/dummy.dds"); }
 
-Title::~Title(){
-	SAFE_DELETE( mImage );
-}
+    Title::~Title() { SAFE_DELETE(mImage); }
 
-Base* Title::update( Parent* parent ){
-	Base* next = this;
-	//获取输入
-	if ( Pad::isTriggered( Pad::U ) ){
-		--mCursorPosistion;
-		if ( mCursorPosistion < 0 ){ //
- 			mCursorPosistion = 1;
-		}
-	}else if ( Pad::isTriggered( Pad::D ) ){
-		++mCursorPosistion;
-		if ( mCursorPosistion > 1 ){ //1越过0就循环成0
-			mCursorPosistion = 0;
-		}
-	}else if ( Pad::isTriggered( Pad::A ) ){
-		next = new Game::Parent( parent->mode() );
-		if ( mCursorPosistion == 0 ){
-			parent->setMode( Parent::MODE_1P );
-		}else if ( mCursorPosistion == 1 ){
-			parent->setMode( Parent::MODE_2P );
-		}else{
-			HALT( "arienai" );
-		}
-	}
-	//绘制
-	mImage->draw();
-	//输出文字
-	Framework f = Framework::instance();
-	f.drawDebugString( 0, 0, "[title] : boom man" );
-	f.drawDebugString( 1, 2, "one player" );
-	f.drawDebugString( 1, 3, "two players" );
-	//写光标
-	f.drawDebugString( 0, mCursorPosistion + 2, ">" );
+    Base* Title::update(Parent* parent) {
+        Base* next = this;
+        // 获取输入
+        if (Pad::isTriggered(Pad::U)) {
+            --mCursorPosistion;
+            if (mCursorPosistion < 0) { //
+                mCursorPosistion = 1;
+            }
+        } else if (Pad::isTriggered(Pad::D)) {
+            ++mCursorPosistion;
+            if (mCursorPosistion > 1) { // 1越过0就循环成0
+                mCursorPosistion = 0;
+            }
+        } else if (Pad::isTriggered(Pad::A)) {
+            next = new Game::Parent(parent->mode());
+            if (mCursorPosistion == 0) {
+                parent->setMode(Parent::MODE_1P);
+            } else if (mCursorPosistion == 1) {
+                parent->setMode(Parent::MODE_2P);
+            } else {
+                HALT("arienai");
+            }
+        }
+        // 绘制
+        mImage->draw();
+        // 输出文字
+        Framework f = Framework::instance();
+        f.drawDebugString(0, 0, "[title] : boom man");
+        f.drawDebugString(1, 2, "one player");
+        f.drawDebugString(1, 3, "two players");
+        // 写光标
+        f.drawDebugString(0, mCursorPosistion + 2, ">");
 
-	return next;
-}
+        return next;
+    }
 
-} //namespace Sequence
+} // namespace Sequence
