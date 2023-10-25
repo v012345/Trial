2ffd36ddf368f311d00bd5171b8977537a0e3188
v012345 <v138468@gmail.com>
2023-10-25 15:59:33 +0800

    格式化

--- example/Sequence/Parent.cpp
+++ example/Sequence/Parent.cpp
@@ -1,55 +1,52 @@
-﻿#include "GameLib/GameLib.h"
-#include "Sequence/Parent.h"
+﻿#include "Sequence/Parent.h"
+#include "GameLib/GameLib.h"
 #include "Sequence/Title.h"
 
-namespace Sequence{
+namespace Sequence {
 
-Parent* Parent::mInstance = 0;
+    Parent* Parent::mInstance = 0;
 
-void Parent::create(){
-	ASSERT( !mInstance );
-	mInstance = new Parent();
-}
+    void Parent::create() {
+        ASSERT(!mInstance);
+        mInstance = new Parent();
+    }
 
-void Parent::destroy(){
-	ASSERT( mInstance );
-	SAFE_DELETE( mInstance );
-}
+    void Parent::destroy() {
+        ASSERT(mInstance);
+        SAFE_DELETE(mInstance);
+    }
 
-Parent* Parent::instance(){
-	return mInstance;
-}
+    Parent* Parent::instance() { //
+        return mInstance;
+    }
 
-Parent::Parent() : 
-mMode( MODE_NONE ),
-mChild( 0 ){
-	//
-	mChild = new Title();
-}
+    Parent::Parent() : mMode(MODE_NONE), mChild(0) { //
+        mChild = new Title();
+    }
 
-Parent::~Parent(){
-	//如果剩余则销毁
-	SAFE_DELETE( mChild );
-}
+    Parent::~Parent() {
+        // 如果剩余则销毁
+        SAFE_DELETE(mChild);
+    }
 
-void Parent::update(){
-	Base* nextChild = mChild->update( this );
-	if ( nextChild != mChild ){
-		//迁移判断
-		Child* casted = dynamic_cast< Child* >( nextChild );
-		ASSERT( casted ); //
-		SAFE_DELETE( mChild );
-		mChild = casted;
-	}
-	nextChild = 0; //以防万一
-}
+    void Parent::update() {
+        Base* nextChild = mChild->update(this);
+        if (nextChild != mChild) {
+            // 迁移判断
+            Child* casted = dynamic_cast<Child*>(nextChild);
+            ASSERT(casted); //
+            SAFE_DELETE(mChild);
+            mChild = casted;
+        }
+        nextChild = 0; // 以防万一
+    }
 
-void Parent::setMode( Mode mode ){
-	mMode = mode;
-}
+    void Parent::setMode(Mode mode) { //
+        mMode = mode;
+    }
 
-Parent::Mode Parent::mode() const {
-	return mMode;
-}
+    Parent::Mode Parent::mode() const { //
+        return mMode;
+    }
 
-} //namespace Sequence
+} // namespace Sequence
