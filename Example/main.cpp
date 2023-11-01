8168f4359c42a8f40d481f808a8f87737ca6cfa8
v012345 <v138468@gmail.com>
2023-11-01 11:23:47 +0800

    碰撞检测

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,31 +1,84 @@
 ﻿#include "GameLib/Framework.h"
-#include "GameLib/Gamelib.h"
-using namespace GameLib;
 
-#include "File.h"
-#include "Image.h"
-#include "Sequence/Parent.h"
-#include "State.h"
+class Square {
+  public:
+    void set(int x, int y, int halfSize) {
+        mX = x;
+        mY = y;
+        mHalfSize = halfSize;
+    }
+    bool isIntersect(const Square& a) const {
+        int la = mX - mHalfSize; // left A
+        int ra = mX + mHalfSize; // right A
+        int lb = a.mX - a.mHalfSize; // left B
+        int rb = a.mX + a.mHalfSize; // right B
+        if ((la < rb) && (ra > lb)) {
+            int ta = mY - mHalfSize; // top A
+            int ba = mY + mHalfSize; // bottom A
+            int tb = a.mY - a.mHalfSize; // top B
+            int bb = a.mY + a.mHalfSize; // bottom B
+            if ((ta < bb) && (ba > tb)) { return true; }
+        }
+        return false;
+    }
+    int mX;
+    int mY;
+    int mHalfSize;
+};
+bool gFirstFrame = true;
+Square gPlayer; // 玩家的意图
+Square gWall; // 墙
 
-// 全局变量
-Sequence::Parent* gRootSequence = 0; // 根序列
-int gCounter = 0; // 一个计数器，用于计算主循环的次数
-
-// 用户封装函数。内容被抛出给mainLoop（）
 namespace GameLib {
     void Framework::update() {
-        if (!gRootSequence) { gRootSequence = new Sequence::Parent(); }
-        // 帧频调整
-        setFrameRate(60); // 只需要调用一次
-
-        if (gCounter % 60 == 0) { // 60每帧显示一次帧率
-            cout << " FrameRate:" << frameRate() << endl;
+        if (gFirstFrame) {
+            setFrameRate(60);
+            gFirstFrame = false;
+            gPlayer.set(16, 16, 16);
+            gWall.set(160, 120, 16);
         }
-        ++gCounter;
-
-        gRootSequence->update();
-        // 结束判断（是否按下q或用鼠标按下X按钮）
-        if (isKeyOn('q')) { requestEnd(); }
-        if (isEndRequested()) { SAFE_DELETE(gRootSequence); }
+        // 获取移动量
+        int dx = 0;
+        int dy = 0;
+        if (isKeyOn('a')) {
+            dx -= 1;
+        } else if (isKeyOn('s')) {
+            dx += 1;
+        }
+        if (isKeyOn('w')) {
+            dy -= 1;
+        } else if (isKeyOn('z')) {
+            dy += 1;
+        }
+        // 移动
+        gPlayer.mX += dx;
+        gPlayer.mY += dy;
+        // 碰撞处理
+        unsigned color;
+        if (gPlayer.isIntersect(gWall)) {
+            color = 0xffffffff; // 碰撞了，给它上色
+        } else {
+            color = 0xffff0000;
+        }
+        // 绘制
+        unsigned* vram = videoMemory();
+        // 清除
+        for (int i = 0; i < width() * height(); ++i) { vram[i] = 0; }
+        //
+        for (int y = 0; y < 32; ++y) {
+            for (int x = 0; x < 32; ++x) {
+                int tx = x + gWall.mX - 16;
+                int ty = y + gWall.mY - 16;
+                vram[ty * width() + tx] = 0xff0000ff;
+            }
+        }
+        //
+        for (int y = 0; y < 32; ++y) {
+            for (int x = 0; x < 32; ++x) {
+                int tx = x + gPlayer.mX - 16;
+                int ty = y + gPlayer.mY - 16;
+                vram[ty * width() + tx] = color;
+            }
+        }
     }
 } // namespace GameLib
