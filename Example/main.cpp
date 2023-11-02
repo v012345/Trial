7daaab6bcc24f2c6f73738e947f208defcbef987
v012345 <v138468@gmail.com>
2023-11-02 11:12:57 +0800

    DrawTexturedTriangle

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,10 +1,29 @@
 ﻿#include "GameLib/Framework.h"
+using namespace GameLib;
+#include "Image.h"
 
-namespace GameLib{
-	void Framework::update(){
-		double p0[ 2 ] = { 100.0, 100.0 };
-		double p1[ 2 ] = { 200.0, 120.0 };
-		double p2[ 2 ] = { 120.0, 200.0 };
-		drawTriangle2D( p0, p1, p2, 0, 0, 0, 0xffff0000, 0xffffffff, 0xff0000ff );
-	}
-}
+Image* gImage;
+Texture* gTexture;
+bool gFirstFrame = true;
+
+namespace GameLib {
+    void Framework::update() {
+        if (gFirstFrame) {
+            gFirstFrame = false;
+            gImage = new Image(CMAKE_CURRENT_SOURCE_DIR "background.dds");
+            createTexture(&gTexture, gImage->width(), gImage->height(), gImage->data(), gImage->width(), gImage->height());
+            SAFE_DELETE(gImage);
+        }
+        setTexture(gTexture);
+        double p0[2] = {100.0, 100.0};
+        double p1[2] = {200.0, 120.0};
+        double p2[2] = {120.0, 200.0};
+        double t0[2] = {0.0, 0.0};
+        double t1[2] = {1.0, 0.0};
+        double t2[2] = {0.0, 1.0};
+        drawTriangle2D(p0, p1, p2, t0, t1, t2);
+
+        // 结束处理
+        if (isEndRequested()) { destroyTexture(&gTexture); }
+    }
+} // namespace GameLib
