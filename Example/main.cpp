52cce02fde5efc7d52324edaa7eced92b36c0e54
v012345 <v138468@gmail.com>
2023-11-02 11:06:27 +0800

    DrawColoredTriangle

--- Example/main.cpp
+++ Example/main.cpp
@@ -5,6 +5,6 @@ namespace GameLib{
 		double p0[ 2 ] = { 100.0, 100.0 };
 		double p1[ 2 ] = { 200.0, 120.0 };
 		double p2[ 2 ] = { 120.0, 200.0 };
-		drawTriangle2D( p0, p1, p2 );
+		drawTriangle2D( p0, p1, p2, 0, 0, 0, 0xffff0000, 0xffffffff, 0xff0000ff );
 	}
 }
