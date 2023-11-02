ced9e024924fe11b121549b897040680585fbb95
v012345 <v138468@gmail.com>
2023-11-02 11:19:43 +0800

    AlphaBlend

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,28 +1,43 @@
 ﻿#include "GameLib/Framework.h"
+#include "GameLib/Input/Manager.h"
+#include "GameLib/Input/Keyboard.h"
 using namespace GameLib;
-#include "Image.h"
 
-Image* gImage;
-Texture* gTexture;
-bool gFirstFrame = true;
+Framework::BlendMode gBlendMode = Framework::BLEND_LINEAR;
+int gCount = 0;
 
-namespace GameLib {
-    void Framework::update() {
-        if (gFirstFrame) {
-            gFirstFrame = false;
-            gImage = new Image(CMAKE_CURRENT_SOURCE_DIR "background.dds");
-            createTexture(&gTexture, gImage->width(), gImage->height(), gImage->data(), gImage->width(), gImage->height());
-            SAFE_DELETE(gImage);
-        }
-        setTexture(gTexture);
-        double p0[2] = {100.0, 100.0};
-        double p1[2] = {200.0, 120.0};
-        double p2[2] = {120.0, 200.0};
-        double t0[2] = {0.0, 0.0};
-        double t1[2] = {1.0, 0.0};
-        double t2[2] = {0.0, 1.0};
-        drawTriangle2D(p0, p1, p2, t0, t1, t2, 0xffffff00, 0xff00ffff, 0xffff00ff);
-        // 结束处理
-        if (isEndRequested()) { destroyTexture(&gTexture); }
-    }
-} // namespace GameLib
+namespace GameLib{
+	void Framework::update(){
+		sleep( 16 );
+		//1第二张不透明
+		setBlendMode( BLEND_OPAQUE );
+		double p00[ 2 ] = { 100.0, 100.0 };
+		double p01[ 2 ] = { 200.0, 120.0 };
+		double p02[ 2 ] = { 120.0, 200.0 };
+		drawTriangle2D( p00, p01, p02, 0, 0, 0, 0xffff8080, 0xff80ff80, 0xff8080ff );
+
+		//2第二张是根据个人喜好。
+		setBlendMode( gBlendMode );  
+		double p10[ 2 ] = { 110.0, 110.0 };
+		double p11[ 2 ] = { 210.0, 130.0 };
+		double p12[ 2 ] = { 130.0, 210.0 };
+		unsigned alpha = ( gCount % 256 ) << 24;
+		++gCount;  
+		drawTriangle2D( 
+			p10, p11, p12, 
+			0, 0, 0, 
+			0x80ff80 | alpha, 
+			0x8080ff | alpha, 
+			0xff8080 | alpha );
+
+		//空格切换
+		if ( Input::Manager::instance().keyboard().isTriggered( ' ' ) ){
+			switch ( gBlendMode ){
+				case BLEND_LINEAR: gBlendMode = BLEND_ADDITIVE; break;
+				case BLEND_ADDITIVE: gBlendMode = BLEND_OPAQUE; break;
+				case BLEND_OPAQUE: gBlendMode = BLEND_LINEAR; break;
+			}
+		}
+		drawDebugString( 0, 0, "press SPACE to change blend mode." );
+	}
+}
