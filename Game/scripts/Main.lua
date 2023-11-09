6e2bd000cc508508e20aa91856fdbae631a67966
v012345 <v138468@gmail.com>
2023-11-09 19:14:21 +0800

    暂存一下

--- Game/scripts/Main.lua
+++ Game/scripts/Main.lua
@@ -25,7 +25,7 @@ xpcall(function()
                 local filePath = folder .. "\\" .. entry
                 local fileAttributes = lfs.attributes(filePath)
                 if fileAttributes.mode == "file" then
-                    if string.match(string.lower(filePath), "^.+%.csd$") and entry ~= "UiNotificationReward.csd" and entry ~= "UiDownload.csd" then
+                    if string.match(string.lower(filePath), "^.+%.csd$") then
                         all_csd_path[entry] = {
                             zhcn = filePath,
                             vi = string.gsub(filePath, "\\zhcn\\", "\\vi\\", 1),
@@ -37,18 +37,55 @@ xpcall(function()
                 end
             end
         end
-        local tableHead = { "zhcn", "en", "id", "th", "vi", "csd", "name", "tag" }
+        local tableHead = { "zhcn", "vi", "th", "id", "en", "csd", "name", "tag" }
         for i, v in ipairs(tableHead) do
             trans:setCell(1, i, v)
         end
         local row_number = 2
+
+        ---comment
+        ---@param csdName any
+        ---@param zhcnCsd XMLNode
+        ---@param viCsd XMLNode
+        ---@param thCsd XMLNode
+        ---@param idCsd XMLNode
+        ---@param enCsd XMLNode
+        local function extract(csdName, zhcnCsd, viCsd, thCsd, idCsd, enCsd)
+            local attributes = { "ButtonText", "LabelText", "PlaceHolderText" }
+            for _, attri in ipairs(attributes) do
+                if zhcnCsd:getAttributeValue(attri) then
+                    trans:setCell(row_number, 1, zhcnCsd:getAttributeValue(attri))
+                    trans:setCell(row_number, 2, viCsd:getAttributeValue(attri))
+                    trans:setCell(row_number, 3, thCsd:getAttributeValue(attri))
+                    trans:setCell(row_number, 4, idCsd:getAttributeValue(attri))
+                    trans:setCell(row_number, 5, enCsd:getAttributeValue(attri))
+                    trans:setCell(row_number, 6, csdName)
+                    trans:setCell(row_number, 7, enCsd:getAttributeValue("Name"))
+                    trans:setCell(row_number, 8, enCsd:getAttributeValue("Tag"))
+                    row_number = row_number + 1
+                end
+            end
+
+            --- 文件结构全部一样
+            for idx, zhcnChild in ipairs(zhcnCsd:getChildren()) do
+                local viChild = viCsd:getChild(idx)
+                local thChild = thCsd:getChild(idx)
+                local idChild = idCsd:getChild(idx)
+                local enChild = enCsd:getChild(idx)
+                extract(csdName, zhcnChild, viChild, thChild, idChild, enChild)
+            end
+        end
+
+        local index = 1
         for csdName, paths in pairs(all_csd_path) do
-            print(csdName)
-            local zhcnCsd = ParseXML(paths.zhcn)
-            local viCsd = ParseXML(paths.vi)
-            local th = ParseXML(paths.th)
-            local idCsd = ParseXML(paths.id)
-            local enCsd = ParseXML(paths.en)
+            print(index, csdName)
+            index = index + 1
+            local zhcnCsd = (ParseXML(paths.zhcn)):getData()
+            local viCsd = (ParseXML(paths.vi)):getData()
+            local thCsd = (ParseXML(paths.th)):getData()
+            local idCsd = (ParseXML(paths.id)):getData()
+            local enCsd = (ParseXML(paths.en)):getData()
+            extract(csdName, zhcnCsd, viCsd, thCsd, idCsd, enCsd)
         end
         trans:writeTo(currentdir .. "\\" .. transFileName)
         print("<<<< 完成提取文字 >>>>")
@@ -68,4 +105,5 @@ xpcall(function()
     os.execute("pause")
 end, function(msg)
     print(msg)
+    os.execute("pause")
 end)
