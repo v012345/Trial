628c0324c350d517796f2e4197f9c09687655f3a
v012345 <v138468@gmail.com>
2023-11-16 14:13:01 +0800

    可以连上服务器

--- windows/Tools/Client/Form1.Designer.cs
+++ windows/Tools/Client/Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            richTextBox1 = new RichTextBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            LblConnectionState = new Label();
+            BtnConnectServer = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(630, 409);
+            button1.Name = "button1";
+            button1.Size = new Size(158, 29);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(450, 409);
+            button2.Name = "button2";
+            button2.Size = new Size(174, 29);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(450, 12);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(338, 391);
+            richTextBox1.TabIndex = 3;
+            richTextBox1.Text = "";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(12, 43);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(117, 24);
+            radioButton1.TabIndex = 4;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(12, 73);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(117, 24);
+            radioButton2.TabIndex = 5;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(12, 103);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(117, 24);
+            radioButton3.TabIndex = 6;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(12, 133);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(117, 24);
+            radioButton4.TabIndex = 7;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "radioButton4";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(12, 163);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(117, 24);
+            radioButton5.TabIndex = 8;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "radioButton5";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // LblConnectionState
+            // 
+            LblConnectionState.AutoSize = true;
+            LblConnectionState.ForeColor = Color.Crimson;
+            LblConnectionState.Location = new Point(12, 383);
+            LblConnectionState.Name = "LblConnectionState";
+            LblConnectionState.Size = new Size(57, 20);
+            LblConnectionState.TabIndex = 9;
+            LblConnectionState.Text = "已断开";
+            LblConnectionState.Click += LblConnectionState_Click;
+            // 
+            // BtnConnectServer
+            // 
+            BtnConnectServer.Location = new Point(12, 406);
+            BtnConnectServer.Name = "BtnConnectServer";
+            BtnConnectServer.Size = new Size(117, 29);
+            BtnConnectServer.TabIndex = 10;
+            BtnConnectServer.Text = "连接服务器";
+            BtnConnectServer.UseVisualStyleBackColor = true;
+            BtnConnectServer.Click += BtnConnectServer_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(BtnConnectServer);
+            Controls.Add(LblConnectionState);
+            Controls.Add(radioButton5);
+            Controls.Add(radioButton4);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(richTextBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private RichTextBox richTextBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton4;
+        private RadioButton radioButton5;
+        private Label LblConnectionState;
+        private Button BtnConnectServer;
     }
 }
--- windows/Tools/Client/Form1.cs
+++ windows/Tools/Client/Form1.cs
@@ -1,10 +1,50 @@
+﻿using System.Net.Sockets;
+using System.Text;
 namespace Client
 {
     public partial class Form1 : Form
     {
+        private string mServerIP = "127.0.0.1";
+        private int mServerPort = 8849;
+        private TcpClient mClient;
+        private NetworkStream mStream;
+        private bool mIsConnecting = false;
         public Form1()
         {
             InitializeComponent();
+            Connect_To_Server();
+
+        }
+        private void Connect_To_Server()
+        {
+            try
+            {
+                // 创建TcpClient对象
+                mClient = new TcpClient(mServerIP, mServerPort);
+                mStream = mClient.GetStream();
+                string message = "Hello from C# client!";
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                mStream.Write(data, 0, data.Length);
+                mIsConnecting = true;
+                LblConnectionState.Text = "已连接";
+                LblConnectionState.ForeColor = Color.Green;
+            }
+            catch (Exception ex)
+            {
+                mIsConnecting = false;
+            }
+        }
+        private void BtnConnectServer_Click(object sender, EventArgs e)
+        {
+            if (!mIsConnecting)
+            {
+                Connect_To_Server();
+            }
+        }
+
+        private void LblConnectionState_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
