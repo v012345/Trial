f7f577a33e1c571fb6a5c5ed201d2d8790762bb3
v012345 <v138468@gmail.com>
2023-11-24 15:28:08 +0800

    先存一下例子, 我不太明白这个例子

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,64 +1,52 @@
 ﻿#include "GameLib/Framework.h"
 #include "GameLib/Input/Keyboard.h"
 #include "GameLib/Input/Manager.h"
+#include <sstream>
+using namespace std;
 
-bool gDepthWrite = true;
-GameLib::Framework::BlendMode gBlendMode = GameLib::Framework::BLEND_LINEAR;
-bool gSwapOrder = false;
+double gZMove = -999.0;
+double gXMove = -0.0;
 
 namespace GameLib {
     void Framework::update() {
         setFrameRate(60);
-        enableDepthTest(true); // Z测试已开启
-        enableDepthWrite(gDepthWrite); // 在这里写入Z缓冲区
-        setBlendMode(gBlendMode);
 
-        double point0[3] = {200.0, 100.0, 1.0};
-        double point1[3] = {400.0, 100.0, 1.0};
-        double point2[3] = {300.0, 400.0, 0.0};
-        unsigned c0 = 0x80ff8080; // 红色的
+        double p[4][3] = {
+            {1500.0, 500.0, 1000.0},
+            {500.0, 1500.0, 1000.0},
+            {1500.0, 500.0, 2000.0},
+            {500.0, 1500.0, 2000.0},
+        };
 
-        double point3[3] = {200.0, 400.0, 1.0};
-        double point4[3] = {400.0, 400.0, 1.0};
-        double point5[3] = {300.0, 100.0, 0.0};
-        unsigned c1 = 0x8080ff80; // 绿
+        // 将x，y除以z，并将z从[1,1000]转换为[0,1]。
+        for (int i = 0; i < 4; ++i) {
+            // 移动量加法
+            p[i][0] += gXMove;
+            p[i][2] += gZMove;
+            p[i][0] /= p[i][2];
+            p[i][1] /= p[i][2];
+            p[i][2] = (1.0 / 9999.0) * p[i][2] - (1.0 / 9999.0);
+        }
+        // 画一个矩形。
+        drawTriangle3D(p[0], p[1], p[2]);
+        drawTriangle3D(p[3], p[1], p[2]);
 
-        if (gSwapOrder) {
-            drawTriangle3D(point3, point4, point5, 0, 0, 0, c1, c1, c1);
-            drawTriangle3D(point0, point1, point2, 0, 0, 0, c0, c0, c0);
-        } else {
-            drawTriangle3D(point0, point1, point2, 0, 0, 0, c0, c0, c0);
-            drawTriangle3D(point3, point4, point5, 0, 0, 0, c1, c1, c1);
+        if (Input::Manager::instance().keyboard().isOn('z')) {
+            gZMove -= 1.f;
+        } else if (Input::Manager::instance().keyboard().isOn('w')) {
+            gZMove += 1.f;
         }
-
-        if (Input::Manager::instance().keyboard().isTriggered('a')) { //
-            gSwapOrder = !gSwapOrder;
+        if (Input::Manager::instance().keyboard().isOn('a')) {
+            gXMove -= 10.f;
+        } else if (Input::Manager::instance().keyboard().isOn('s')) {
+            gXMove += 10.f;
         }
-        if (Input::Manager::instance().keyboard().isTriggered('s')) {
-            if (gBlendMode == BLEND_ADDITIVE) {
-                gBlendMode = BLEND_LINEAR;
-            } else {
-                gBlendMode = BLEND_ADDITIVE;
-            }
-        }
-        if (Input::Manager::instance().keyboard().isTriggered('d')) { //
-            gDepthWrite = !gDepthWrite;
-        }
-
-        if (gSwapOrder) {
-            drawDebugString(0, 0, "[a] Swap Order : On");
-        } else {
-            drawDebugString(0, 0, "[a] Swap Order : Off");
-        }
-        if (gBlendMode == BLEND_LINEAR) {
-            drawDebugString(0, 1, "[s] Linear Blend");
-        } else {
-            drawDebugString(0, 1, "[s] Additive Blend");
-        }
-        if (gDepthWrite) {
-            drawDebugString(0, 2, "[d] Depth Write : On");
-        } else {
-            drawDebugString(0, 2, "[d] Depth Write : Off");
-        }
+        // 显示
+        ostringstream oss;
+        oss << "[w-z] Z MOVEMENT: " << gZMove;
+        drawDebugString(10, 0, oss.str().c_str());
+        oss.str("");
+        oss << "[a-s] X MOVEMENT: " << gXMove;
+        drawDebugString(10, 1, oss.str().c_str());
     }
 } // namespace GameLib
--- scripts/Game/Main.lua
+++ scripts/Game/Main.lua
@@ -7,69 +7,39 @@ xpcall(function()
     local gDepthWrite = true;
     local gSwapOrder = false;
     local gBlendMode = Enum.BlendMode.BLEND_LINEAR;
-
-    Framework:setFrameRate(60)
+    local gZMove = -999.0
+    local gXMove = -0.0
+    -- Framework:setFrameRate(60)
     function MainLoop()
         xpcall(function()
-            Framework:enableDepthTest(gDepthTest)
-            Framework:enableDepthWrite(gDepthWrite);
-            Framework:setBlendMode(gBlendMode);
-            local p0 = { 200.0, 100.0, 1.0 }
-            local p1 = { 400.0, 100.0, 1.0 }
-            local p2 = { 300.0, 400.0, -0.5 }
-            local c1 = 0x80ff0000
-            local p3 = { 200.0, 400.0, 1.5 }
-            local p4 = { 400.0, 400.0, 1.0 }
-            local p5 = { 300.0, 100.0, 0.0 }
-            local c2 = 0x8000ff00
-            if gSwapOrder then
-                Framework:drawTriangle3D(p3, p4, p5, 0, 0, 0, c2, c2, c2);
-                Framework:drawTriangle3D(p0, p1, p2, 0, 0, 0, c1, c1, c1);
-            else
-                Framework:drawTriangle3D(p0, p1, p2, 0, 0, 0, c1, c1, c1);
-                Framework:drawTriangle3D(p3, p4, p5, 0, 0, 0, c2, c2, c2);
+            local p = {
+                { 1500.0, 500.0,  1000.0 },
+                { 500.0,  1500.0, 1000.0 },
+                { 1500.0, 500.0,  2000.0 },
+                { 500.0,  1500.0, 2000.0 },
+            }
+            for i = 1, 4 do
+                p[i][1] = p[i][1] + gXMove
+                p[i][3] = p[i][3] + gZMove
+                p[i][1] = p[i][1] / p[i][3]
+                p[i][2] = p[i][2] / p[i][3]
+                p[i][3] = (1.0 / 9999.0) * p[i][3] - (1.0 / 9999.0)
             end
 
-
-            if Framework:isKeyTriggered(Enum.Keyboard.Space) then
-                gDepthTest = not gDepthTest
+            Framework:drawTriangle3D(p[1], p[2], p[3]);
+            Framework:drawTriangle3D(p[4], p[2], p[3]);
+            if Framework:isKeyOn(Enum.Keyboard.S) then
+                gZMove = gZMove - 1
+            elseif Framework:isKeyOn(Enum.Keyboard.W) then
+                gZMove = gZMove + 1
             end
-            if gDepthTest then
-                Framework:drawDebugString(0, 0, "Depth Test : On (press space)")
-            else
-                Framework:drawDebugString(0, 0, "Depth Test : Off (press space)")
+            if Framework:isKeyOn(Enum.Keyboard.A) then
+                gXMove = gXMove - 1
+            elseif Framework:isKeyOn(Enum.Keyboard.D) then
+                gXMove = gXMove + 1
             end
-
-            if Framework:isKeyTriggered(Enum.Keyboard.D) then
-                gDepthWrite = not gDepthWrite
-            end
-            if gDepthWrite then
-                Framework:drawDebugString(0, 2, "[d] Depth Write : On");
-            else
-                Framework:drawDebugString(0, 2, "[d] Depth Write : Off");
-            end
-            if Framework:isKeyTriggered(Enum.Keyboard.S) then
-                gSwapOrder = not gSwapOrder
-            end
-            if gSwapOrder then
-                Framework:drawDebugString(0, 1, "[s] draw Green first");
-            else
-                Framework:drawDebugString(0, 1, "[s] draw Red first");
-            end
-
-            if Framework:isKeyTriggered(Enum.Keyboard.A) then
-                if gBlendMode == Enum.BlendMode.BLEND_LINEAR then
-                    gBlendMode = Enum.BlendMode.BLEND_ADDITIVE
-                else
-                    gBlendMode = Enum.BlendMode.BLEND_LINEAR
-                end
-            end
-            if gBlendMode == Enum.BlendMode.BLEND_LINEAR then
-                Framework:drawDebugString(0, 3, "[a] Linear Blend");
-            else
-                Framework:drawDebugString(0, 3, "[a] Additive Blend");
-            end
-
+            Framework:drawDebugString(10, 0, "[w-s] Z MOVEMENT: " .. gZMove)
+            Framework:drawDebugString(10, 1, "[a-d] X MOVEMENT: " .. gXMove)
             Framework:drawDebugString(70, 0, "FPS : " .. Framework:frameRate())
         end, function(msg)
             print(msg)
