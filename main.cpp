d6126d444d381e2c29d2c8c87fd4d72d974829eb
v012345 <v138468@gmail.com>
2023-11-28 18:39:56 +0800

    Prog2_2_point

--- main.cpp
+++ main.cpp
@@ -2,21 +2,56 @@
 //
 #include <GLFW\glfw3.h>
 #include <iostream>
-
 using namespace std;
 
-void init(GLFWwindow* window) {}
+#define numVAOs 1
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() {
+    const char* vshaderSource = "#version 430    \n"
+                                "void main(void) \n"
+                                "{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+
+    const char* fshaderSource = "#version 430    \n"
+                                "out vec4 color; \n"
+                                "void main(void) \n"
+                                "{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
+
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+    GLuint vfprogram = glCreateProgram();
+
+    glShaderSource(vShader, 1, &vshaderSource, NULL);
+    glShaderSource(fShader, 1, &fshaderSource, NULL);
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+
+    glAttachShader(vfprogram, vShader);
+    glAttachShader(vfprogram, fShader);
+    glLinkProgram(vfprogram);
+
+    return vfprogram;
+}
+
+void init(GLFWwindow* window) {
+    renderingProgram = createShaderProgram();
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+}
 
 void display(GLFWwindow* window, double currentTime) {
-    glClearColor(1.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(renderingProgram);
+    glPointSize(30.0f);
+    glDrawArrays(GL_POINTS, 0, 1);
 }
 
 int main(void) {
     if (!glfwInit()) { exit(EXIT_FAILURE); }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 2 - program 1", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 2 - program 2", NULL, NULL);
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
     glfwSwapInterval(1);
