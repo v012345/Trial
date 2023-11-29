8caa81957ecd0f240dad328efeda21a8dc8e1f80
v012345 <v138468@gmail.com>
2023-11-29 17:49:52 +0800

    Prog4_1_1_plainRedCube

--- main.cpp
+++ main.cpp
@@ -1,29 +1,103 @@
 #include <GL\glew.h>
 //
+#include "Utils\Utils.h"
 #include <GLFW\glfw3.h>
+#include <cmath>
+#include <fstream>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm\gtc\type_ptr.hpp> // glm::value_ptr
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-void init(GLFWwindow* window) {
-    int size;
-    float size_[2];
+#define numVAOs 1
+#define numVBOs 2
 
-    glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &size);
-    glGetFloatv(GL_POINT_SIZE_RANGE, size_);
+Utils util = Utils();
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
 
-    cout << size << endl;
-    cout << size_[0] << endl;
-    cout << size_[1] << endl;
+// variable allocation for display
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat;
+
+void setupVertices(void) {
+    float vertexPositions[108] = {
+        -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, //
+        1.0f,  -1.0f, -1.0f, 1.0f,  1.0f,  -1.0f, -1.0f, 1.0f,  -1.0f, //
+        1.0f,  -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  -1.0f, //
+        1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  -1.0f, //
+        1.0f,  -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f,  1.0f,  1.0f,  1.0f, //
+        -1.0f, -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,  1.0f,  1.0f,  1.0f, //
+        -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,  1.0f, //
+        -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,  -1.0f, -1.0f, 1.0f,  1.0f, //
+        -1.0f, -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  1.0f,  -1.0f, -1.0f, //
+        1.0f,  -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, //
+        -1.0f, 1.0f,  -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  1.0f,  1.0f, //
+        1.0f,  1.0f,  1.0f,  -1.0f, 1.0f,  1.0f,  -1.0f, 1.0f,  -1.0f //
+    };
+
+    glGenVertexArrays(1, vao);
+    glBindVertexArray(vao[0]);
+    glGenBuffers(numVBOs, vbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
 }
 
-void display(GLFWwindow* window, double currentTime) {}
+void init(GLFWwindow* window) {
+    renderingProgram = Utils::createShaderProgram(SHADERS_DIR "vertShader.glsl", SHADERS_DIR "fragShader.glsl");
+    cameraX = 0.0f;
+    cameraY = 0.0f;
+    cameraZ = 8.0f;
+    cubeLocX = 0.0f;
+    cubeLocY = -2.0f;
+    cubeLocZ = 0.0f;
+    setupVertices();
+}
+
+void display(GLFWwindow* window, double currentTime) {
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(renderingProgram);
+
+    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+    projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+
+    glfwGetFramebufferSize(window, &width, &height);
+    aspect = (float)width / (float)height;
+    pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+
+    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+    mvMat = vMat * mMat;
+
+    glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+}
 
 int main(void) {
     if (!glfwInit()) { exit(EXIT_FAILURE); }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 2 - supplemental notes", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 4 - program 1", NULL, NULL);
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
     glfwSwapInterval(1);
--- shaders/vertShader.glsl
+++ shaders/vertShader.glsl
@@ -1,12 +1,10 @@
 #version 430
 
-uniform float offset;
+layout(location = 0) in vec3 position;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
 
 void main(void) {
-  if(gl_VertexID == 0)
-    gl_Position = vec4(0.25 + offset, -0.25, 0.0, 1.0);
-  else if(gl_VertexID == 1)
-    gl_Position = vec4(-0.25 + offset, -0.25, 0.0, 1.0);
-  else
-    gl_Position = vec4(0.25 + offset, 0.25, 0.0, 1.0);
+  gl_Position = proj_matrix * mv_matrix * vec4(position, 1.0);
 }
