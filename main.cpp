1233a0432df94c35aa03cb91ed07ba45fac94b20
v012345 <v138468@gmail.com>
2023-11-29 12:02:17 +0800

    Prog2_supplemental_queryingOpenGL

--- main.cpp
+++ main.cpp
@@ -1,46 +1,29 @@
 #include <GL\glew.h>
 //
-#include "Utils/Utils.h"
 #include <GLFW\glfw3.h>
-#include <fstream>
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-#define numVAOs 1
-
-GLuint renderingProgram;
-GLuint vao[numVAOs];
-GLuint offsetLoc;
-float x = 0.0f;
-float inc = 0.01f;
-
 void init(GLFWwindow* window) {
-    renderingProgram = Utils::createShaderProgram(SHADERS_DIR "vertShader.glsl", SHADERS_DIR "fragShader.glsl");
-    glGenVertexArrays(numVAOs, vao);
-    glBindVertexArray(vao[0]);
+    int size;
+    float size_[2];
+
+    glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &size);
+    glGetFloatv(GL_POINT_SIZE_RANGE, size_);
+
+    cout << size << endl;
+    cout << size_[0] << endl;
+    cout << size_[1] << endl;
 }
 
-void display(GLFWwindow* window, double currentTime) {
-    glClear(GL_DEPTH_BUFFER_BIT);
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glUseProgram(renderingProgram);
-
-    x += inc;
-    if (x > 1.0f) inc = -0.01f;
-    if (x < -1.0f) inc = 0.01f;
-    offsetLoc = glGetUniformLocation(renderingProgram, "offset");
-    glProgramUniform1f(renderingProgram, offsetLoc, x);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-}
+void display(GLFWwindow* window, double currentTime) {}
 
 int main(void) {
     if (!glfwInit()) { exit(EXIT_FAILURE); }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 2 - program 5", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 2 - supplemental notes", NULL, NULL);
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
     glfwSwapInterval(1);
