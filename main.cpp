26e7c0924aeb975733e3988a483e08aead25babb
v012345 <v138468@gmail.com>
2023-11-30 12:10:02 +0800

    Prog4_2_instancing

--- main.cpp
+++ main.cpp
@@ -20,11 +20,10 @@ GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 
 // variable allocation for display
-GLuint mvLoc, projLoc;
-int width, height, displayLoopi;
-float aspect;
-float tf;
-glm::mat4 pMat, vMat, tMat, rMat, mMat, mvMat;
+GLuint mLoc, vLoc, projLoc, tfLoc;
+int width, height;
+float aspect, timeFactor;
+glm::mat4 pMat, vMat, mMat, mvMat;
 
 void setupVertices(void) {
     float vertexPositions[108] = {
@@ -59,7 +58,7 @@ void init(GLFWwindow* window) {
 
     cameraX = 0.0f;
     cameraY = 0.0f;
-    cameraZ = 32.0f;
+    cameraZ = 420.0f; // Z=32.0f when 24 instances, 420.0f when 100000 instances
     setupVertices();
 }
 
@@ -70,33 +69,26 @@ void display(GLFWwindow* window, double currentTime) {
 
     glUseProgram(renderingProgram);
 
-    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+    vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
     projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
 
     vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
 
+    glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
-    for (displayLoopi = 0; displayLoopi < 24; displayLoopi++) {
-        tf = currentTime + displayLoopi;
-        tMat = glm::translate(glm::mat4(1.0f), glm::vec3(sin(.35f * tf) * 8.0f, cos(.52f * tf) * 8.0f, sin(.70f * tf) * 8.0f));
-        rMat = glm::rotate(glm::mat4(1.0f), 1.75f * tf, glm::vec3(0.0f, 1.0f, 0.0f));
-        rMat = glm::rotate(rMat, 1.75f * tf, glm::vec3(1.0f, 0.0f, 0.0f));
-        rMat = glm::rotate(rMat, 1.75f * tf, glm::vec3(0.0f, 0.0f, 1.0f));
-        mMat = tMat * rMat;
-        mvMat = vMat * mMat;
+    timeFactor = ((float)currentTime);
+    tfLoc = glGetUniformLocation(renderingProgram, "tf");
+    glUniform1f(tfLoc, (float)timeFactor);
 
-        glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+    glEnableVertexAttribArray(0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-        glEnableVertexAttribArray(0);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
 
-        glEnable(GL_DEPTH_TEST);
-        glDepthFunc(GL_LEQUAL);
-
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
+    glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 100000); // 0, 36, 24  (or 100000)
 }
 
 void window_size_callback(GLFWwindow* win, int newWidth, int newHeight) {
@@ -109,7 +101,7 @@ int main(void) {
     if (!glfwInit()) { exit(EXIT_FAILURE); }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 4 - program 1d", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 4 - program 2", NULL, NULL);
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
     glfwSwapInterval(1);
