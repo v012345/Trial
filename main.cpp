f7f7adaa7c7d38eb444d6ab37258a1f9d5571d88
v012345 <v138468@gmail.com>
2023-12-01 11:37:12 +0800

    Prog6_2_torus

--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 #include <GL\glew.h>
 //
-#include "Utils\Sphere.h"
+#include "Utils\Torus.h"
 #include "Utils\Utils.h"
 #include <GLFW\glfw3.h>
 #include <SOIL2\soil2.h>
-#include <cmath>
 #include <fstream>
-#include <glm\glm.hpp>
 #include <glm\gtc\matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
 #include <glm\gtc\type_ptr.hpp> // glm::value_ptr
 #include <iostream>
 #include <string>
 using namespace std;
 
+float toRadians(float degrees) { return (degrees * 2.0f * 3.14159f) / 360.0f; }
+
 #define numVAOs 1
-#define numVBOs 3
+#define numVBOs 4
 
 float cameraX, cameraY, cameraZ;
-float sphLocX, sphLocY, sphLocZ;
+float torLocX, torLocY, torLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
-GLuint earthTexture;
+GLuint brickTexture;
 float rotAmt = 0.0f;
 
 // variable allocation for display
@@ -30,30 +30,28 @@ int width, height;
 float aspect;
 glm::mat4 pMat, vMat, mMat, mvMat;
 
-Sphere mySphere = Sphere(48);
+Torus myTorus(0.5f, 0.2f, 48);
 
 void setupVertices(void) {
-    std::vector<int> ind = mySphere.getIndices();
-    std::vector<glm::vec3> vert = mySphere.getVertices();
-    std::vector<glm::vec2> tex = mySphere.getTexCoords();
-    std::vector<glm::vec3> norm = mySphere.getNormals();
+    std::vector<int> ind = myTorus.getIndices();
+    std::vector<glm::vec3> vert = myTorus.getVertices();
+    std::vector<glm::vec2> tex = myTorus.getTexCoords();
+    std::vector<glm::vec3> norm = myTorus.getNormals();
 
     std::vector<float> pvalues;
     std::vector<float> tvalues;
     std::vector<float> nvalues;
 
-    int numIndices = mySphere.getNumIndices();
-    for (int i = 0; i < numIndices; i++) {
-        pvalues.push_back((vert[ind[i]]).x);
-        pvalues.push_back((vert[ind[i]]).y);
-        pvalues.push_back((vert[ind[i]]).z);
-        tvalues.push_back((tex[ind[i]]).s);
-        tvalues.push_back((tex[ind[i]]).t);
-        nvalues.push_back((norm[ind[i]]).x);
-        nvalues.push_back((norm[ind[i]]).y);
-        nvalues.push_back((norm[ind[i]]).z);
+    for (int i = 0; i < myTorus.getNumVertices(); i++) {
+        pvalues.push_back(vert[i].x);
+        pvalues.push_back(vert[i].y);
+        pvalues.push_back(vert[i].z);
+        tvalues.push_back(tex[i].s);
+        tvalues.push_back(tex[i].t);
+        nvalues.push_back(norm[i].x);
+        nvalues.push_back(norm[i].y);
+        nvalues.push_back(norm[i].z);
     }
-
     glGenVertexArrays(1, vao);
     glBindVertexArray(vao[0]);
     glGenBuffers(numVBOs, vbo);
@@ -66,6 +64,9 @@ void setupVertices(void) {
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
     glBufferData(GL_ARRAY_BUFFER, nvalues.size() * 4, &nvalues[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[3]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, ind.size() * 4, &ind[0], GL_STATIC_DRAW);
 }
 
 void init(GLFWwindow* window) {
@@ -73,16 +74,16 @@ void init(GLFWwindow* window) {
     cameraX = 0.0f;
     cameraY = 0.0f;
     cameraZ = 2.0f;
-    sphLocX = 0.0f;
-    sphLocY = 0.0f;
-    sphLocZ = -1.0f;
+    torLocX = 0.0f;
+    torLocY = 0.0f;
+    torLocZ = -0.5f;
 
     glfwGetFramebufferSize(window, &width, &height);
     aspect = (float)width / (float)height;
     pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
 
     setupVertices();
-    earthTexture = Utils::loadTexture(RES_DIR "earth.jpg");
+    brickTexture = Utils::loadTexture(RES_DIR "brick1.jpg");
 }
 
 void display(GLFWwindow* window, double currentTime) {
@@ -96,7 +97,10 @@ void display(GLFWwindow* window, double currentTime) {
     projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
 
     vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
-    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(sphLocX, sphLocY, sphLocZ));
+    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(torLocX, torLocY, torLocZ));
+    // mMat *= glm::eulerAngleXYZ(toRadians(30.0f), 0.0f, 0.0f);
+    mMat = glm::rotate(mMat, toRadians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
     mvMat = vMat * mMat;
 
     glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
@@ -111,12 +115,13 @@ void display(GLFWwindow* window, double currentTime) {
     glEnableVertexAttribArray(1);
 
     glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, earthTexture);
+    glBindTexture(GL_TEXTURE_2D, brickTexture);
 
     glEnable(GL_CULL_FACE);
     glFrontFace(GL_CCW);
 
-    glDrawArrays(GL_TRIANGLES, 0, mySphere.getNumIndices());
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[3]);
+    glDrawElements(GL_TRIANGLES, myTorus.getIndices().size(), GL_UNSIGNED_INT, 0);
 }
 
 void window_size_callback(GLFWwindow* win, int newWidth, int newHeight) {
@@ -129,7 +134,7 @@ int main(void) {
     if (!glfwInit()) { exit(EXIT_FAILURE); }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter6 - program1", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Chapter6 - program2", NULL, NULL);
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
     glfwSwapInterval(1);
