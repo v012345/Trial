e730988890eef66cdbe49080a57c24728d325c59
v012345 <v138468@gmail.com>
2023-12-04 14:45:00 +0800

    Prog10_4_1_heightMappedTerrain

--- shaders/vertShader.glsl
+++ shaders/vertShader.glsl
@@ -1,49 +1,18 @@
 #version 430
 
-layout (location = 0) in vec3 vertPos;
-layout (location = 1) in vec2 texCoord;
-layout (location = 2) in vec3 vertNormal;
-layout (location = 3) in vec3 vertTangent;
+layout (location=0) in vec3 vertPos;
+layout (location=1) in vec2 texCoord;
+layout (location=2) in vec3 vertNormal;
 
-out vec3 varyingLightDir;
-out vec3 varyingVertPos;
-out vec3 varyingNormal;
-out vec3 varyingTangent;
-out vec3 originalVertex;
 out vec2 tc;
 
-layout (binding=0) uniform sampler2D s;
-layout (binding=1) uniform sampler2D t;
-
-struct PositionalLight
-{	vec4 ambient;
-	vec4 diffuse;
-	vec4 specular;
-	vec3 position;
-};
-struct Material
-{	vec4 ambient;
-	vec4 diffuse;
-	vec4 specular;
-	float shininess;
-};
-
-uniform vec4 globalAmbient;
-uniform PositionalLight light;
-uniform Material material;
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
-uniform mat4 norm_matrix;
+layout (binding=0) uniform sampler2D t;	// for texture
+layout (binding=1) uniform sampler2D h;	// for height map
 
 void main(void)
-{	varyingVertPos = (mv_matrix * vec4(vertPos,1.0)).xyz;
-	varyingLightDir = light.position - varyingVertPos;
+{	vec4 p = vec4(vertPos,1.0) + vec4((vertNormal*((texture(h, texCoord).r)/5.0f)),1.0f);
 	tc = texCoord;
-	
-	originalVertex = vertPos;
-
-	varyingNormal = (norm_matrix * vec4(vertNormal,1.0)).xyz;
-	varyingTangent = (norm_matrix * vec4(vertTangent,1.0)).xyz;
-
-	gl_Position = proj_matrix * mv_matrix * vec4(vertPos,1.0);
+	gl_Position = proj_matrix * mv_matrix * p;
 }
