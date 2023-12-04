01cfb01bca3cd8fda2834478c3e58ce83a5b8afb
v012345 <v138468@gmail.com>
2023-12-04 10:49:02 +0800

    Prog8_1_shadowMapping

--- main.cpp
+++ main.cpp
@@ -5,38 +5,34 @@
 #include <GLFW\glfw3.h>
 #include <SOIL2\soil2.h>
 #include <fstream>
-#include <glm\gtc\matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
-#include <glm\gtc\type_ptr.hpp> // glm::value_ptr
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
 #include <iostream>
 #include <string>
+//
+#include "Utils\ImportedModel.h"
 using namespace std;
+
+void passOne(void);
+void passTwo(void);
 
 float toRadians(float degrees) { return (degrees * 2.0f * 3.14159f) / 360.0f; }
 
 #define numVAOs 1
-#define numVBOs 4
+#define numVBOs 5
 
-float cameraX, cameraY, cameraZ;
-float torLocX, torLocY, torLocZ;
-GLuint renderingProgram;
+GLuint renderingProgram1, renderingProgram2;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 
-Torus myTorus(0.5f, 0.2f, 48);
-int numTorusVertices = myTorus.getNumVertices();
-int numTorusIndices = myTorus.getNumIndices();
+ImportedModel pyramid(RES_DIR "pyr.obj");
+Torus myTorus(0.6f, 0.4f, 48);
+int numPyramidVertices, numTorusVertices, numTorusIndices;
 
-glm::vec3 lightLoc = glm::vec3(5.0f, 2.0f, 2.0f);
-float amt = 0.0f;
-
-// variable allocation for display
-GLuint mvLoc, projLoc, nLoc;
-GLuint globalAmbLoc, ambLoc, diffLoc, specLoc, posLoc, mambLoc, mdiffLoc, mspecLoc, mshiLoc;
-int width, height;
-float aspect;
-glm::mat4 pMat, vMat, mMat, mvMat, invTrMat, rMat;
-glm::vec3 currentLightPos, transformed;
-float lightPos[3];
+glm::vec3 torusLoc(1.6f, 0.0f, -0.3f);
+glm::vec3 pyrLoc(-1.0f, 0.1f, 0.3f);
+glm::vec3 cameraLoc(0.0f, 0.2f, 6.0f);
+glm::vec3 lightLoc(-3.8f, 2.2f, 1.1f);
 
 // white light
 float globalAmbient[4] = {0.7f, 0.7f, 0.7f, 1.0f};
@@ -45,16 +41,59 @@ float lightDiffuse[4] = {1.0f, 1.0f, 1.0f, 1.0f};
 float lightSpecular[4] = {1.0f, 1.0f, 1.0f, 1.0f};
 
 // gold material
-float* matAmb = Utils::goldAmbient();
-float* matDif = Utils::goldDiffuse();
-float* matSpe = Utils::goldSpecular();
-float matShi = Utils::goldShininess();
+float* gMatAmb = Utils::goldAmbient();
+float* gMatDif = Utils::goldDiffuse();
+float* gMatSpe = Utils::goldSpecular();
+float gMatShi = Utils::goldShininess();
 
-void installLights(glm::mat4 vMatrix) {
+// bronze material
+float* bMatAmb = Utils::bronzeAmbient();
+float* bMatDif = Utils::bronzeDiffuse();
+float* bMatSpe = Utils::bronzeSpecular();
+float bMatShi = Utils::bronzeShininess();
+
+float thisAmb[4], thisDif[4], thisSpe[4], matAmb[4], matDif[4], matSpe[4];
+float thisShi, matShi;
+
+// shadow stuff
+int scSizeX, scSizeY;
+GLuint shadowTex, shadowBuffer;
+glm::mat4 lightVmatrix;
+glm::mat4 lightPmatrix;
+glm::mat4 shadowMVP1;
+glm::mat4 shadowMVP2;
+glm::mat4 b;
+
+// variable allocation for display
+GLuint mvLoc, projLoc, nLoc, sLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat, invTrMat;
+glm::vec3 currentLightPos, transformed;
+float lightPos[3];
+GLuint globalAmbLoc, ambLoc, diffLoc, specLoc, posLoc, mambLoc, mdiffLoc, mspecLoc, mshiLoc;
+glm::vec3 origin(0.0f, 0.0f, 0.0f);
+glm::vec3 up(0.0f, 1.0f, 0.0f);
+
+void installLights(int renderingProgram, glm::mat4 vMatrix) {
     transformed = glm::vec3(vMatrix * glm::vec4(currentLightPos, 1.0));
     lightPos[0] = transformed.x;
     lightPos[1] = transformed.y;
     lightPos[2] = transformed.z;
+
+    matAmb[0] = thisAmb[0];
+    matAmb[1] = thisAmb[1];
+    matAmb[2] = thisAmb[2];
+    matAmb[3] = thisAmb[3];
+    matDif[0] = thisDif[0];
+    matDif[1] = thisDif[1];
+    matDif[2] = thisDif[2];
+    matDif[3] = thisDif[3];
+    matSpe[0] = thisSpe[0];
+    matSpe[1] = thisSpe[1];
+    matSpe[2] = thisSpe[2];
+    matSpe[3] = thisSpe[3];
+    matShi = thisShi;
 
     // get the locations of the light and material fields in the shader
     globalAmbLoc = glGetUniformLocation(renderingProgram, "globalAmbient");
@@ -80,87 +119,218 @@ void installLights(glm::mat4 vMatrix) {
 }
 
 void setupVertices(void) {
-    std::vector<int> ind = myTorus.getIndices();
-    std::vector<glm::vec3> vert = myTorus.getVertices();
-    std::vector<glm::vec2> tex = myTorus.getTexCoords();
-    std::vector<glm::vec3> norm = myTorus.getNormals();
 
-    std::vector<float> pvalues;
-    std::vector<float> tvalues;
-    std::vector<float> nvalues;
+    // pyramid definition
 
-    for (int i = 0; i < myTorus.getNumVertices(); i++) {
-        pvalues.push_back(vert[i].x);
-        pvalues.push_back(vert[i].y);
-        pvalues.push_back(vert[i].z);
-        tvalues.push_back(tex[i].s);
-        tvalues.push_back(tex[i].t);
-        nvalues.push_back(norm[i].x);
-        nvalues.push_back(norm[i].y);
-        nvalues.push_back(norm[i].z);
+    numPyramidVertices = pyramid.getNumVertices();
+    std::vector<glm::vec3> vert = pyramid.getVertices();
+    std::vector<glm::vec3> norm = pyramid.getNormals();
+
+    std::vector<float> pyramidPvalues;
+    std::vector<float> pyramidNvalues;
+
+    for (int i = 0; i < numPyramidVertices; i++) {
+        pyramidPvalues.push_back((vert[i]).x);
+        pyramidPvalues.push_back((vert[i]).y);
+        pyramidPvalues.push_back((vert[i]).z);
+        pyramidNvalues.push_back((norm[i]).x);
+        pyramidNvalues.push_back((norm[i]).y);
+        pyramidNvalues.push_back((norm[i]).z);
     }
+
+    // torus definition
+
+    numTorusVertices = myTorus.getNumVertices();
+    numTorusIndices = myTorus.getNumIndices();
+    std::vector<int> ind = myTorus.getIndices();
+    vert = myTorus.getVertices();
+    norm = myTorus.getNormals();
+
+    std::vector<float> torusPvalues;
+    std::vector<float> torusNvalues;
+
+    for (int i = 0; i < numTorusVertices; i++) {
+        torusPvalues.push_back(vert[i].x);
+        torusPvalues.push_back(vert[i].y);
+        torusPvalues.push_back(vert[i].z);
+        torusNvalues.push_back(norm[i].x);
+        torusNvalues.push_back(norm[i].y);
+        torusNvalues.push_back(norm[i].z);
+    }
+
     glGenVertexArrays(1, vao);
     glBindVertexArray(vao[0]);
     glGenBuffers(numVBOs, vbo);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-    glBufferData(GL_ARRAY_BUFFER, pvalues.size() * 4, &pvalues[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, torusPvalues.size() * 4, &torusPvalues[0], GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-    glBufferData(GL_ARRAY_BUFFER, tvalues.size() * 4, &tvalues[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, pyramidPvalues.size() * 4, &pyramidPvalues[0], GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
-    glBufferData(GL_ARRAY_BUFFER, nvalues.size() * 4, &nvalues[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, torusNvalues.size() * 4, &torusNvalues[0], GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[3]);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[3]);
+    glBufferData(GL_ARRAY_BUFFER, pyramidNvalues.size() * 4, &pyramidNvalues[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[4]);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, ind.size() * 4, &ind[0], GL_STATIC_DRAW);
 }
 
+void setupShadowBuffers(GLFWwindow* window) {
+    glfwGetFramebufferSize(window, &width, &height);
+    scSizeX = width;
+    scSizeY = height;
+
+    glGenFramebuffers(1, &shadowBuffer);
+
+    glGenTextures(1, &shadowTex);
+    glBindTexture(GL_TEXTURE_2D, shadowTex);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32, scSizeX, scSizeY, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+
+    // may reduce shadow border artifacts
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+}
+
 void init(GLFWwindow* window) {
-    renderingProgram = Utils::createShaderProgram(SHADERS_DIR "GouraudShaders/vertShader.glsl", SHADERS_DIR "GouraudShaders/fragShader.glsl");
-    cameraX = 0.0f;
-    cameraY = 0.0f;
-    cameraZ = 1.0f;
-    torLocX = 0.0f;
-    torLocY = 0.0f;
-    torLocZ = -1.0f;
+    renderingProgram1 = Utils::createShaderProgram(SHADERS_DIR "vert1Shader.glsl", SHADERS_DIR "frag1Shader.glsl");
+    renderingProgram2 = Utils::createShaderProgram(SHADERS_DIR "vert2Shader.glsl", SHADERS_DIR "frag2Shader.glsl");
 
     glfwGetFramebufferSize(window, &width, &height);
     aspect = (float)width / (float)height;
     pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
 
     setupVertices();
+    setupShadowBuffers(window);
+
+    b = glm::mat4(0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.5f, 0.0f, 0.5f, 0.5f, 0.5f, 1.0f);
 }
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 void display(GLFWwindow* window, double currentTime) {
     glClear(GL_DEPTH_BUFFER_BIT);
-    glClearColor(0.0, 0.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glUseProgram(renderingProgram);
+    currentLightPos = glm::vec3(lightLoc);
 
-    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
-    projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
-    nLoc = glGetUniformLocation(renderingProgram, "norm_matrix");
+    lightVmatrix = glm::lookAt(currentLightPos, origin, up);
+    lightPmatrix = glm::perspective(toRadians(60.0f), aspect, 0.1f, 1000.0f);
 
-    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+    glBindFramebuffer(GL_FRAMEBUFFER, shadowBuffer);
+    glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, shadowTex, 0);
 
-    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(torLocX, torLocY, torLocZ));
-    mMat *= glm::rotate(mMat, toRadians(35.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    glDrawBuffer(GL_NONE);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_POLYGON_OFFSET_FILL); // for reducing
+    glPolygonOffset(2.0f, 4.0f); //  shadow artifacts
 
-    currentLightPos = glm::vec3(lightLoc.x, lightLoc.y, lightLoc.z);
-    amt += 0.5f;
-    rMat = glm::rotate(glm::mat4(1.0f), toRadians(amt), glm::vec3(0.0f, 0.0f, 1.0f));
-    currentLightPos = glm::vec3(rMat * glm::vec4(currentLightPos, 1.0f));
+    passOne();
 
-    installLights(vMat);
+    glDisable(GL_POLYGON_OFFSET_FILL); // artifact reduction, continued
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, shadowTex);
+
+    glDrawBuffer(GL_FRONT);
+
+    passTwo();
+}
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+void passOne(void) {
+    glUseProgram(renderingProgram1);
+
+    // draw the torus
+
+    mMat = glm::translate(glm::mat4(1.0f), torusLoc);
+    mMat = glm::rotate(mMat, toRadians(25.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+    shadowMVP1 = lightPmatrix * lightVmatrix * mMat;
+    sLoc = glGetUniformLocation(renderingProgram1, "shadowMVP");
+    glUniformMatrix4fv(sLoc, 1, GL_FALSE, glm::value_ptr(shadowMVP1));
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[4]);
+    glDrawElements(GL_TRIANGLES, numTorusIndices, GL_UNSIGNED_INT, 0);
+
+    // draw the pyramid
+
+    mMat = glm::translate(glm::mat4(1.0f), pyrLoc);
+    mMat = glm::rotate(mMat, toRadians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    mMat = glm::rotate(mMat, toRadians(40.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    shadowMVP1 = lightPmatrix * lightVmatrix * mMat;
+    glUniformMatrix4fv(sLoc, 1, GL_FALSE, glm::value_ptr(shadowMVP1));
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glDrawArrays(GL_TRIANGLES, 0, numPyramidVertices);
+}
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+void passTwo(void) {
+    glUseProgram(renderingProgram2);
+
+    mvLoc = glGetUniformLocation(renderingProgram2, "mv_matrix");
+    projLoc = glGetUniformLocation(renderingProgram2, "proj_matrix");
+    nLoc = glGetUniformLocation(renderingProgram2, "norm_matrix");
+    sLoc = glGetUniformLocation(renderingProgram2, "shadowMVP");
+
+    // draw the torus
+
+    thisAmb[0] = bMatAmb[0];
+    thisAmb[1] = bMatAmb[1];
+    thisAmb[2] = bMatAmb[2]; // bronze
+    thisDif[0] = bMatDif[0];
+    thisDif[1] = bMatDif[1];
+    thisDif[2] = bMatDif[2];
+    thisSpe[0] = bMatSpe[0];
+    thisSpe[1] = bMatSpe[1];
+    thisSpe[2] = bMatSpe[2];
+    thisShi = bMatShi;
+
+    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraLoc.x, -cameraLoc.y, -cameraLoc.z));
+
+    mMat = glm::translate(glm::mat4(1.0f), torusLoc);
+    mMat = glm::rotate(mMat, toRadians(25.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+    currentLightPos = glm::vec3(lightLoc);
+    installLights(renderingProgram2, vMat);
 
     mvMat = vMat * mMat;
-    invTrMat = glm::transpose(glm::inverse(mvMat)); // 向量的变换是逆换吗?
+    invTrMat = glm::transpose(glm::inverse(mvMat));
+    shadowMVP2 = b * lightPmatrix * lightVmatrix * mMat;
 
     glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
     glUniformMatrix4fv(nLoc, 1, GL_FALSE, glm::value_ptr(invTrMat));
+    glUniformMatrix4fv(sLoc, 1, GL_FALSE, glm::value_ptr(shadowMVP2));
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
@@ -170,13 +340,58 @@ void display(GLFWwindow* window, double currentTime) {
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(1);
 
+    glClear(GL_DEPTH_BUFFER_BIT);
     glEnable(GL_CULL_FACE);
     glFrontFace(GL_CCW);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[3]);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[4]);
     glDrawElements(GL_TRIANGLES, numTorusIndices, GL_UNSIGNED_INT, 0);
+
+    // draw the pyramid
+
+    thisAmb[0] = gMatAmb[0];
+    thisAmb[1] = gMatAmb[1];
+    thisAmb[2] = gMatAmb[2]; // gold
+    thisDif[0] = gMatDif[0];
+    thisDif[1] = gMatDif[1];
+    thisDif[2] = gMatDif[2];
+    thisSpe[0] = gMatSpe[0];
+    thisSpe[1] = gMatSpe[1];
+    thisSpe[2] = gMatSpe[2];
+    thisShi = gMatShi;
+
+    mMat = glm::translate(glm::mat4(1.0f), pyrLoc);
+    mMat = glm::rotate(mMat, toRadians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    mMat = glm::rotate(mMat, toRadians(40.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    currentLightPos = glm::vec3(lightLoc);
+    installLights(renderingProgram2, vMat);
+
+    mvMat = vMat * mMat;
+    invTrMat = glm::transpose(glm::inverse(mvMat));
+    shadowMVP2 = b * lightPmatrix * lightVmatrix * mMat;
+
+    glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+    glUniformMatrix4fv(nLoc, 1, GL_FALSE, glm::value_ptr(invTrMat));
+    glUniformMatrix4fv(sLoc, 1, GL_FALSE, glm::value_ptr(shadowMVP2));
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[3]);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(1);
+
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glDrawArrays(GL_TRIANGLES, 0, numPyramidVertices);
 }
 
 void window_size_callback(GLFWwindow* win, int newWidth, int newHeight) {
@@ -189,7 +404,7 @@ int main(void) {
     if (!glfwInit()) { exit(EXIT_FAILURE); }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Chapter7 - program1", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Chapter8 - program1", NULL, NULL);
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
     glfwSwapInterval(1);
