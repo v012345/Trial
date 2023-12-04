ba7ac313b2f09ef37313b1b31bc525519939d628
v012345 <v138468@gmail.com>
2023-12-04 16:23:52 +0800

    Prog13_1_1_geomModInflate

--- shaders/vertShader.glsl
+++ shaders/vertShader.glsl
@@ -1,23 +1,41 @@
 #version 430
 
-out vec2 tc;
+layout (location=0) in vec4 vertPos;
+layout (location=1) in vec4 vertNormal;
 
-uniform mat4 mvp_matrix;
-layout (binding = 0) uniform sampler2D tex_color;
-layout (binding = 1) uniform sampler2D tex_height;
+out vec3 varyingNormal; 
+out vec3 varyingLightDir;
+out vec3 varyingHalfVector;
+
+struct PositionalLight
+{	vec4 ambient;
+	vec4 diffuse;
+	vec4 specular;
+	vec3 position;
+};
+struct Material
+{	vec4 ambient;  
+	vec4 diffuse;  
+	vec4 specular;  
+	float shininess;
+};
+
+uniform vec4 globalAmbient;
+uniform PositionalLight light;
+uniform Material material;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform mat4 norm_matrix;
 
 void main(void)
-{	const vec2 patchTexCoords[] =
-		vec2[] (vec2(0,0), vec2(1,0), vec2(0,1), vec2(1,1));
+{
+	// output vertex positions, light, and normal vectors to the rasterizer for interpolation
+	vec3 vertPos3 = (mv_matrix * vertPos).xyz;
+	varyingLightDir = light.position - vertPos3;
+	varyingNormal = (norm_matrix * vertNormal).xyz;
 	
-	// compute an offset for coordinates based on which instance this is
-	int x = gl_InstanceID % 64;
-	int y = gl_InstanceID / 64;
+	// calculate the half vector (L+V)
+	varyingHalfVector = normalize(varyingLightDir) + normalize(-vertPos3);
 	
-	// texture coordinates are distributed across 64 patches	
-	tc = vec2( (x+patchTexCoords[gl_VertexID].x)/64.0,
-			(63-y+patchTexCoords[gl_VertexID].y)/64.0);
-	
-	// vertex locations range from -0.5 to +0.5
-	gl_Position = vec4(tc.x-0.5, 0.0, (1.0-tc.y)-0.5, 1.0);
+	gl_Position = mv_matrix * vertPos;
 }
