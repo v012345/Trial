f6009a1e152fb34c441c8e17f9428639f9e51418
v012345 <v138468@gmail.com>
2023-12-04 16:09:05 +0800

    Prog12_3_2_tessHeightMappedInstWLighting

--- shaders/tessCShader.glsl
+++ shaders/tessCShader.glsl
@@ -1,11 +1,27 @@
 #version 430
 
 layout (vertices = 4) out;
+
 in vec2 tc[];
 out vec2 tcs_out[];
 
 uniform mat4 mvp_matrix;
 layout (binding=0) uniform sampler2D tex_color;
+layout (binding = 1) uniform sampler2D tex_height;
+layout (binding = 2) uniform sampler2D tex_normal;
+
+/*--- light stuff----*/
+struct PositionalLight
+{	vec4 ambient; vec4 diffuse; vec4 specular; vec3 position; };
+struct Material
+{	vec4 ambient; vec4 diffuse; vec4 specular; float shininess; };
+uniform vec4 globalAmbient;
+uniform PositionalLight light;
+uniform Material material;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform mat4 norm_matrix;
+/*-----------------*/
 
 void main(void)
 {	int TL = 32;
--- shaders/vertShader.glsl
+++ shaders/vertShader.glsl
@@ -4,6 +4,21 @@ out vec2 tc;
 
 uniform mat4 mvp_matrix;
 layout (binding = 0) uniform sampler2D tex_color;
+layout (binding = 1) uniform sampler2D tex_height;
+layout (binding = 2) uniform sampler2D tex_normal;
+
+/* --- light stuff ----*/
+struct PositionalLight
+{ vec4 ambient; vec4 diffuse; vec4 specular; vec3 position; };
+struct Material
+{ vec4 ambient; vec4 diffuse; vec4 specular; float shininess; };
+uniform vec4 globalAmbient;
+uniform PositionalLight light;
+uniform Material material;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform mat4 norm_matrix;
+/*-----------------*/
 
 void main(void)
 {	vec2 patchTexCoords[] =
