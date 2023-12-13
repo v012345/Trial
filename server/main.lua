43ed8f44a79af17bea59048ed10a0a72037e62a0
v012345 <v138468@gmail.com>
2023-12-13 13:54:11 +0800

    加入服务器逻辑

--- server/main.lua
+++ server/main.lua
@@ -1,27 +1,48 @@
-require "Tools.Debugger"
-local x = 1
-local y = 12
-local z = 123
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(1)
-print(2)
-print(2)
-print(2)
-print(2)
-print(2)
-print(x)
+-- xpcall(function(...)
+--     require "get"
+--     local file = io.open("2.txt", "w")
+--     if file then
+--         print('KKK')
+--         file:write("22")
+--         getbyhttp("http://localhost:8080/1.txt", file)
+
+--         file:close()
+--     end
+-- end, function(msg)
+--     print(msg)
+-- end)
+-- local json = require "Tools.JSON"
+local socket = require("socket")
+
+local host = "2.tcp.cpolar.cn"
+local port = 14731
+
+-- local host = "192.168.1.6"
+-- local port = 3389
+
+-- 创建 TCP 客户端
+local client = socket.tcp()
+
+-- 连接到服务器
+client:connect(host, port)
+
+-- 发送 HTTP GET 请求
+local c = {}
+c.cmd = "update"
+c.source = "https://raw.githubusercontent.com/v012345/Trial/master/server/main.lua"
+c.target = "main.lua"
+local request = '{"cma}update\n'
+client:send(request)
+
+-- 读取服务器响应
+local response, err = client:receive("*a")
+
+if response then
+    print("Response body:")
+    print(response)
+else
+    print("Error:", err)
+end
+
+-- 关闭连接
+client:close()
